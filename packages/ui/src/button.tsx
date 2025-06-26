e1c1d838cfc9b583cef28a5a4ac0e408226b73f3
ljh0608 <m98m@naver.com>
2025-06-26 17:48:58 +0900

    init: tailwind cn utils 추가

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
+import { cn } from "./lib/cn";
 
 interface ButtonProps {
   children: ReactNode;
@@ -9,9 +10,13 @@ interface ButtonProps {
 }
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
+  console.log(className);
   return (
     <button
-      className={className}
+      className={cn(
+        "px-4 py-2 rounded-md font-medium transition-colors bg-blue text-white hover:bg-color-pink-500",
+        className
+      )}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
