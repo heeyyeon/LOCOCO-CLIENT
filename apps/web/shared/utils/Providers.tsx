1eb3b20fdc040cf43dd8cfd162829923dc2ed29c
Jeong-Ag <kjungu1105@gmail.com>
2025-06-30 22:51:38 +0900

    feat: `Providers` 컴포넌트 추가

---
+++ apps/web/shared/utils/Providers.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PropsWithChildren } from 'react';
+import { getQueryClient } from './get-query-client';
+
+export function Providers({ children }: PropsWithChildren) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
