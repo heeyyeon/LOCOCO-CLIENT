47c33873f0dc73356c073166fd513dd45c5e7a29
Jeong-Ag <kjungu1105@gmail.com>
2025-07-01 16:40:13 +0900

    feat: shadcn 예시 Select 추가

---
+++ packages/design-system/src/components/ui/select/Select.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+} from './Select';
+
+const meta: Meta<typeof Select> = {
+  title: 'components/Select',
+  component: Select,
+  tags: ['autodocs'],
+};
+export default meta;
+
+export const Ddefault: StoryObj<typeof Select> = {
+  render: (args) => (
+    <Select {...args}>
+      <SelectTrigger>
+        <SelectValue placeholder="Select a fruit" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Fruits</SelectLabel>
+          <SelectItem value="apple">Apple</SelectItem>
+          <SelectItem value="banana">Banana</SelectItem>
+          <SelectItem value="blueberry">Blueberry</SelectItem>
+          <SelectSeparator />
+          <SelectItem value="grapes">Grapes</SelectItem>
+          <SelectItem value="pineapple">Pineapple</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  ),
+  args: {},
+};
