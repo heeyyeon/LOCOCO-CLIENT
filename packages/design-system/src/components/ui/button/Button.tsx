ead26b4cc621a9696b0d45e2971dbc9ee783409e
Jeong-Ag <kjungu1105@gmail.com>
2025-07-01 16:39:13 +0900

    refactor: shadcn 구조에 맞춰 button 컴포넌트, cn 함수 이동

---
+++ packages/design-system/src/components/ui/button/Button.tsx
@@ -0,0 +1,25 @@
+import { ComponentProps } from 'react';
+import { cn } from '../../../lib/utils';
+
+interface ButtonProps extends ComponentProps<'button'> {
+  size?: 'small' | 'large';
+}
+
+export default function Button({
+  size = 'small',
+  disabled = false,
+  children,
+  ...rest
+}: ButtonProps) {
+  const baseStyle =
+    'rounded-md font-medium bg-pink-200 transition-colors duration-200 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-white';
+
+  const sizeStyle =
+    size === 'large' ? 'text-lg px-6 py-3' : 'text-sm px-4 py-2';
+
+  return (
+    <button className={cn(baseStyle, sizeStyle)} disabled={disabled} {...rest}>
+      {children}
+    </button>
+  );
+}
