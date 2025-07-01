66097d1f65899099072424952acf36d1fef17472
Jeong-Ag <kjungu1105@gmail.com>
2025-07-01 16:39:13 +0900

    refactor: shadcn 구조에 맞춰 button 컴포넌트, cn 함수 이동

--- packages/design-system/src/components/ui/button/Button.stories.ts
+++ packages/design-system/src/components/ui/button/Button.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Button from './Button';
 
 const meta: Meta<typeof Button> = {
-  title: 'stories/Button',
+  title: 'components/Button',
   component: Button,
   tags: ['autodocs'],
 };
@@ -10,6 +10,12 @@ export default meta;
 
 type Story = StoryObj<typeof Button>;
 
+export const Small: Story = {
+  args: {
+    size: 'small',
+    children: 'small button',
+  },
+};
 export const Large: Story = {
   args: {
     size: 'large',
--- packages/design-system/src/components/ui/button/Button.tsx
+++ packages/design-system/src/components/ui/button/Button.tsx
@@ -1,5 +1,5 @@
 import { ComponentProps } from 'react';
-import { cn } from '../../lib/utils';
+import { cn } from '../../../lib/utils';
 
 interface ButtonProps extends ComponentProps<'button'> {
   size?: 'small' | 'large';
