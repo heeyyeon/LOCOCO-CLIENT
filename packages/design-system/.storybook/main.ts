ff4ebf9043659f833379a88e4d0b32b0deb7f449
ljh0608 <m98m@naver.com>
2025-07-02 15:00:38 +0900

    init: eslint storybook config 추가로 lint에러 제거

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -1,33 +1,32 @@
-import type { StorybookConfig } from "@storybook/react-vite";
-
-import { join, dirname } from "path";
+import type { StorybookConfig } from '@storybook/react-vite';
+import { dirname, join } from 'path';
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value: string): string {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-docs"),
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-vitest"),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-docs'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath('@storybook/addon-vitest'),
   ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
   docs: {
-    defaultName: "Documentation",
+    defaultName: 'Documentation',
   },
   typescript: {
     check: true, // 타입 체크 활성화
-    reactDocgen: "react-docgen-typescript", // props 자동 추출 도구
+    reactDocgen: 'react-docgen-typescript', // props 자동 추출 도구
   },
 };
 export default config;
