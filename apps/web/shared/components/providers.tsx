60439b8749042584658b44ab187d2fe093033d93
Jeong-Ag <kjungu1105@gmail.com>
2025-07-02 14:22:34 +0900

    feat: `providers` 파일명 수정 후 위치 변경

--- apps/web/shared/components/providers.tsx
+++ apps/web/shared/components/providers.tsx
@@ -3,7 +3,7 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { PropsWithChildren } from 'react';
-import { getQueryClient } from './get-query-client';
+import { getQueryClient } from '../utils';
 
 export function Providers({ children }: PropsWithChildren) {
   const queryClient = getQueryClient();
