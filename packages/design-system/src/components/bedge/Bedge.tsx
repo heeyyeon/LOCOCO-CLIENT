c73b95b37237f5e903f0a73a69e73e60c6848b73
kojesung <js95112345@gmail.com>
2025-07-05 00:42:05 +0900

    chore: 주석 삭제 및 interface명 오타 수정

--- packages/design-system/src/components/bedge/Bedge.tsx
+++ packages/design-system/src/components/bedge/Bedge.tsx
@@ -1,19 +1,18 @@
+import { cva } from 'class-variance-authority';
 import { cn } from '../../lib/utils';
 
-interface BadgeProps {
+interface BedgeProps {
   rank: string;
 }
 
-export default function Badge({ rank }: BadgeProps) {
+export default function Badge({ rank }: BedgeProps) {
   const isFirst = rank === '1';
 
   return (
     <div
       className={cn(
-        // 공통 베이스 스타일
         'text-en-title2 flex h-[3.6rem] w-[3.6rem] items-center justify-center font-[700]',
         'absolute left-0 top-0 z-50',
-        // 조건부 스타일
         isFirst
           ? 'bg-pink-500 text-white'
           : 'border-b-[0.1rem] border-r-[0.1rem] border-pink-500 bg-pink-100 text-pink-500'
