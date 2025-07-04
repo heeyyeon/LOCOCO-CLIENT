5c8b0c35e7fb21a9804d88da7dc5a97cd977adf4
kojesung <js95112345@gmail.com>
2025-07-04 22:56:22 +0900

    feat: cn 함수 활용 공통으로 사용되는 style 분리

--- packages/design-system/src/components/bedge/Bedge.tsx
+++ packages/design-system/src/components/bedge/Bedge.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import { cn } from '../../lib/utils';
 
-interface BedgeProps {
+interface BadgeProps {
   rank: string;
 }
 
-export default function Bedge({ rank }: BedgeProps) {
-  if (rank === '1') {
-    return (
-      <div className="text-en-title2 flex h-[3.6rem] w-[3.6rem] items-center justify-center bg-pink-500 font-[700] text-white">
-        {rank}
-      </div>
-    );
-  } else {
-    return (
-      <div className="text-en-title2 flex h-[3.6rem] w-[3.6rem] items-center justify-center border-b-[0.1rem] border-r-[0.1rem] border-pink-500 bg-pink-100 font-[700] text-pink-500">
-        {rank}
-      </div>
-    );
-  }
+export default function Badge({ rank }: BadgeProps) {
+  const isFirst = rank === '1';
+
+  return (
+    <div
+      className={cn(
+        // 공통 베이스 스타일
+        'text-en-title2 flex h-[3.6rem] w-[3.6rem] items-center justify-center font-[700]',
+        // 조건부 스타일
+        isFirst
+          ? 'bg-pink-500 text-white'
+          : 'border-b-[0.1rem] border-r-[0.1rem] border-pink-500 bg-pink-100 text-pink-500'
+      )}
+    >
+      {rank}
+    </div>
+  );
 }
