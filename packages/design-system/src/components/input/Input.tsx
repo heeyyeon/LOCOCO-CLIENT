106016e80a6954fbf07a84904bf12080914b2bb7
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-07-05 17:46:37 +0900

    refactor: 에러메시지 분리 및 코드리뷰 반영

--- packages/design-system/src/components/input/Input.tsx
+++ packages/design-system/src/components/input/Input.tsx
@@ -1,43 +1,30 @@
 import { cva } from 'class-variance-authority';
 import * as React from 'react';
-import { SvgErrorFill } from '../../icons/fill/components/ErrorFill';
 import { cn } from '../../lib/utils';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  variant?: 'default' | 'search';
-  error?: string;
+  type?: 'default' | 'search';
 }
 
 const inputVariants = cva('w-[25.5rem] focus:outline-none', {
   variants: {
-    variant: {
+    type: {
       default:
         'h-[3.25rem] border-b border-b-gray-400 text-jp-body2 hover:border-b-pink-500 focus:border-b-pink-500',
-      search: 'h-[4rem] text-right text-jp-title1 font-bold',
+      search: 'h-[4rem] text-right text-jp-title1 font-bold focus:bg-gray-50',
     },
   },
   defaultVariants: {
-    variant: 'default',
+    type: 'default',
   },
 });
 
 export default function Input({
-  variant = 'default',
-  error,
+  type = 'default',
   className,
   ...props
 }: InputProps) {
-  const classes = cn(inputVariants({ variant }), className);
-
   return (
-    <div>
-      <input className={classes} {...props} />
-      {error && (
-        <p className="text-jp-caption3 mt-[0.5rem] flex items-center text-[color:var(--color-red)]">
-          <SvgErrorFill className="mr-[0.5rem] h-[0.83rem] w-[0.83rem] fill-[color:var(--color-red)]" />
-          {error}
-        </p>
-      )}
-    </div>
+    <input className={cn(inputVariants({ type }), className)} {...props} />
   );
 }
