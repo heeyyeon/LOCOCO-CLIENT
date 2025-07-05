a6588fe2ba51eadf1b66fadc3cc6da2a73f8d5e9
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-05 17:11:33 +0900

    refactor: icon props 수정

--- packages/design-system/src/icons/fill/components/ArrowRight.tsx
+++ packages/design-system/src/icons/fill/components/ArrowRight.tsx
@@ -5,19 +5,19 @@ interface Props extends SVGProps<SVGSVGElement> {
   fill?: string;
 }
 
-export function SvgArrowRight(props: Props) {
+export function SvgArrowRight({ size = 16, fill = '#5F5F67', ...rest }: Props) {
   return (
     <svg
-      width={props.size || 16}
-      height={props.size || 16}
+      width={size}
+      height={size}
       viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
+      {...rest}
     >
       <path
         d="M5.53027 11.06L8.58361 8L5.53027 4.94L6.47027 4L10.4703 8L6.47027 12L5.53027 11.06Z"
-        fill={props.fill || '#5F5F67'}
+        fill={fill}
       />
     </svg>
   );
