1db4b40e597a97bb4ab7d0751c8cfda14e7bd756
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-05 16:16:08 +0900

    feat: footer icon 추가

--- packages/design-system/src/icons/fill/components/index.tsx
+++ packages/design-system/src/icons/fill/components/index.tsx
@@ -3,3 +3,6 @@ export { SvgArrowUp } from './ArrowUp';
 export { SvgCheck } from './Check';
 export { SvgErrorFill } from './ErrorFill';
 export { SvgLikeFill } from './LikeFill';
+export { SvgInstagramFill } from './InstagramFill';
+export { SvgTwitterFill } from './Twitter_fill';
+export { SvgArrowRight } from './ArrowRight';
