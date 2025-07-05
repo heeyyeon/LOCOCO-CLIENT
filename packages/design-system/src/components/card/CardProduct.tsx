a7042e0aac3a858b6ddf3c410e51753b2fa1f183
kojesung <js95112345@gmail.com>
2025-07-05 21:28:42 +0900

    feat: 상품 카드 svg 클릭 이벤트 개선

--- packages/design-system/src/components/card/CardProduct.tsx
+++ packages/design-system/src/components/card/CardProduct.tsx
@@ -15,7 +15,7 @@ interface CardProductProps {
   reviewCount?: number;
   likeCount: number;
   imageUrl?: string;
-  handleLikeToggle?: (productId: number) => void;
+  handleLikeToggle?: (productId: number, isLiked: boolean) => void;
   handleCardClick?: (productId: number) => void;
 }
 
@@ -33,6 +33,11 @@ export default function CardProduct({
   handleCardClick,
   handleLikeToggle,
 }: CardProductProps) {
+  const handleLikeClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    handleLikeToggle?.(productId, isLiked);
+  };
+
   return (
     <article
       className="flex w-[16.5rem] cursor-pointer flex-col"
@@ -40,7 +45,11 @@ export default function CardProduct({
     >
       <div className="relative border-[0.0625rem] border-gray-200">
         {imageUrl ? (
-          <img className="h-[16.5rem] w-[16.5rem]" src={imageUrl} />
+          <img
+            className="h-[16.5rem] w-[16.5rem] object-cover"
+            src={imageUrl}
+            alt={title}
+          />
         ) : (
           <div className="flex h-[16.5rem] w-[16.5rem] items-center justify-center">
             상품 이미지 준비중
@@ -50,15 +59,13 @@ export default function CardProduct({
       </div>
       <div className="flex h-[2.75rem] items-center justify-between border-b-[0.0625rem] border-dashed border-pink-500">
         <p className="text-jp-body1 font-[700]">{brand}</p>
-        {isLiked ? (
-          <SvgLikeFill size={44} fill="rgba(255, 72, 143, 1)" />
-        ) : (
-          <SvgLikeOutline
-            size={44}
-            fill="white"
-            onClick={() => handleLikeToggle?.(productId)}
-          />
-        )}
+        <button onClick={handleLikeClick}>
+          {isLiked ? (
+            <SvgLikeFill size={44} fill="#ff487f" />
+          ) : (
+            <SvgLikeOutline size={44} fill="white" />
+          )}
+        </button>
       </div>
       <div className="flex h-[2.75rem] items-center border-b-[0.0625rem] border-dashed border-pink-500">
         <p className="text-jp-body2 font-[500]">{title}</p>
@@ -66,7 +73,7 @@ export default function CardProduct({
       <div className="text-en-caption1 flex h-[2.75rem] items-center justify-between border-b-[0.0625rem] border-pink-500 text-gray-600">
         <p>{description}</p>
         <div className="flex">
-          <SvgStar size={16} fill="rgba(254, 195, 65, 1)" />{' '}
+          <SvgStar size={16} fill="rgba(254, 195, 65, 1)" />
           <div className="flex">
             <p>{rating}/5</p>
             <p>({reviewCount})</p>
