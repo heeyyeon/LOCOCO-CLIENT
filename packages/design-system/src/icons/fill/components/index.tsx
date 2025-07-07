83b2a170337300d6ee21c6a0664b41a4c37c796a
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-07 16:16:40 +0900

    feat: icon 추가

--- packages/design-system/src/icons/fill/components/index.tsx
+++ packages/design-system/src/icons/fill/components/index.tsx
@@ -1,3 +1,8 @@
+export { SvgLogo } from './Logo';
 export { SvgArrowUp } from './ArrowUp';
 export { SvgErrorFill } from './ErrorFill';
+export { SvgHistory } from './History';
 export { SvgLikeFill } from './LikeFill';
+export { SvgLogin } from './Login';
+export { SvgMy } from './My';
+export { SvgSearch } from './Search';
