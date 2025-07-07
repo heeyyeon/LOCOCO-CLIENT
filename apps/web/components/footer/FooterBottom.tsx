b5d00a841095e2665f0753a8c4ee949bc50f0382
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-07 12:09:15 +0900

    refactor: footer 파일 내부에 right/left/bottom 위치 시키기

--- apps/web/components/footer/FooterBottom.tsx
+++
@@ -1,9 +0,0 @@
-import { FOOTER_COPYRIGHT } from '../../constants/footer';
-
-export default function FooterBottom() {
-  return (
-    <p className="text-en-body1 bg-[var(--color-pink-100)] font-medium text-[color:var(--color-pink-500)]">
-      {FOOTER_COPYRIGHT}
-    </p>
-  );
-}
