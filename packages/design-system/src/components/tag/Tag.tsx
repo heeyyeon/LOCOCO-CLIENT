53b4fed18209779603c24ac5f03fb32f795c9b18
kojesung <js95112345@gmail.com>
2025-07-08 16:52:46 +0900

    feat: text 태그로 감싸기

--- packages/design-system/src/components/tag/Tag.tsx
+++ packages/design-system/src/components/tag/Tag.tsx
@@ -8,7 +8,7 @@ export default function Tag({ text }: TagProps) {
   return (
     <div className="text-jp-caption1 inline-flex items-center justify-center gap-[0.4rem] rounded-[0.4rem] bg-gray-100 px-[0.8rem] py-[0.45rem] font-[500] text-gray-800">
       <SvgCheckNonBg className="fill-green" size={20} />
-      {text}
+      <p>{text}</p>
     </div>
   );
 }
