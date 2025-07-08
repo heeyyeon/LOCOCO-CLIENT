63a567e4e8009ff1cb32c5463aa850c933a7f505
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-08 22:40:03 +0900

    refactor: category option 타입 구조 변경

--- apps/web/types/category.ts
+++ apps/web/types/category.ts
@@ -5,4 +5,7 @@ export type CategoryKey = keyof typeof CATEGORY_OPTIONS;
 export type CategoryName = (typeof CATEGORY_NAME)[CategoryKey];
 
 export type CategoryOption =
-  (typeof CATEGORY_OPTIONS)[CategoryKey][keyof (typeof CATEGORY_OPTIONS)[CategoryKey]];
+  | (typeof CATEGORY_OPTIONS)['FACIAL_CARE'][keyof (typeof CATEGORY_OPTIONS)['FACIAL_CARE']]
+  | (typeof CATEGORY_OPTIONS)['FACE_MAKEUP'][keyof (typeof CATEGORY_OPTIONS)['FACE_MAKEUP']]
+  | (typeof CATEGORY_OPTIONS)['EYE_MAKEUP'][keyof (typeof CATEGORY_OPTIONS)['EYE_MAKEUP']]
+  | (typeof CATEGORY_OPTIONS)['LIP_MAKEUP'][keyof (typeof CATEGORY_OPTIONS)['LIP_MAKEUP']];
