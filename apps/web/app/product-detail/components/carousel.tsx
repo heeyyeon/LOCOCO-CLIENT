178a364054aed79d458605d3e801576e19e1222b
ljh0608 <m98m@naver.com>
2025-07-09 02:13:42 +0900

    design: 상품 상세페이지 메인 캐러셀 디자인

---
+++ apps/web/app/product-detail/components/carousel.tsx
@@ -0,0 +1,159 @@
+'use client';
+
+import type { Swiper as SwiperType } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState, useRef } from 'react';
+import Image from 'next/image';
+import IconButton from '@/components/icon-button';
+import { SvgArrowUp } from '@/icons';
+import { SvgArrowDown } from '@/icons';
+
+export default function Carousel() {
+  const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
+  const mainSwiperRef = useRef<SwiperType | null>(null);
+
+  return (
+    <div className="flex gap-6 p-4">
+      {/* Thumbnail Swiper (Vertical) */}
+      <div className="flex-shrink-1 flex flex-col justify-between">
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          direction="vertical"
+          spaceBetween={10}
+          slidesPerView={5}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className="mySwiper vertical"
+        >
+          <SwiperSlide>
+            <Image
+              src={'/images/swiper5.png'}
+              alt="swiper thumbnail"
+              width={60}
+              height={60}
+              className="h-full w-full object-cover"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src={'/images/swiper1.png'}
+              alt="swiper thumbnail"
+              width={60}
+              height={60}
+              className="h-full w-full object-cover"
+            />
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Image
+              src={'/images/swiper2.png'}
+              alt="swiper thumbnail"
+              width={60}
+              height={60}
+              className="h-full w-full object-cover"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src={'/images/swiper3.png'}
+              alt="swiper thumbnail"
+              width={60}
+              height={60}
+              className="h-full w-full object-cover"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src={'/images/swiper4.png'}
+              alt="swiper thumbnail"
+              width={60}
+              height={60}
+              className="h-full w-full object-cover"
+            />
+          </SwiperSlide>
+        </Swiper>
+        {/* 화살표 버튼 */}
+        <div className="mt-4 flex flex-col gap-1">
+          <IconButton
+            onClick={() => mainSwiperRef.current?.slidePrev()}
+            aria-label="이전 이미지"
+            icon={<SvgArrowUp />}
+          />
+          <IconButton
+            onClick={() => mainSwiperRef.current?.slideNext()}
+            aria-label="다음 이미지"
+            icon={<SvgArrowDown />}
+          />
+        </div>
+      </div>
+
+      {/* Main Swiper */}
+      <div className="relative flex">
+        <Swiper
+          spaceBetween={30}
+          navigation={false}
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className="mainCustomSwiper"
+          onSwiper={(swiper) => {
+            mainSwiperRef.current = swiper;
+          }}
+        >
+          <SwiperSlide>
+            <Image
+              src={'/images/swiper5.png'}
+              alt="swiper main"
+              width={600}
+              height={400}
+              className="h-full w-full object-cover"
+            />
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Image
+              src={'/images/swiper1.png'}
+              alt="swiper main"
+              width={600}
+              height={400}
+              className="h-full w-full object-cover"
+            />
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Image
+              src={'/images/swiper2.png'}
+              alt="swiper main"
+              width={600}
+              height={400}
+              className="h-full w-full object-cover"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src={'/images/swiper3.png'}
+              alt="swiper main"
+              width={600}
+              height={400}
+              className="h-full w-full object-cover"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src={'/images/swiper4.png'}
+              alt="swiper main"
+              width={600}
+              height={400}
+              className="h-full w-full object-cover"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+}
