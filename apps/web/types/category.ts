f5a8b5bd62fe2e2101a960c4b9c47d30d69975ba
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-08 21:31:04 +0900

    refactor: category option type 수정

--- apps/web/types/category.ts
+++ apps/web/types/category.ts
@@ -5,7 +5,4 @@ export type CategoryKey = keyof typeof CATEGORY_OPTIONS;
 export type CategoryName = (typeof CATEGORY_NAME)[CategoryKey];
 
 export type CategoryOption =
-  | (typeof CATEGORY_OPTIONS)['FACIAL_CARE'][keyof (typeof CATEGORY_OPTIONS)['FACIAL_CARE']]
-  | (typeof CATEGORY_OPTIONS)['FACE_MAKEUP'][keyof (typeof CATEGORY_OPTIONS)['FACE_MAKEUP']]
-  | (typeof CATEGORY_OPTIONS)['EYE_MAKEUP'][keyof (typeof CATEGORY_OPTIONS)['EYE_MAKEUP']]
-  | (typeof CATEGORY_OPTIONS)['LIP_MAKEUP'][keyof (typeof CATEGORY_OPTIONS)['LIP_MAKEUP']];
+  (typeof CATEGORY_OPTIONS)[CategoryKey][keyof (typeof CATEGORY_OPTIONS)[CategoryKey]];
