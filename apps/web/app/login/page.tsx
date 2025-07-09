c0ef0b0f7b9869c72ce9f57007526c5c2a3edf82
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-07-10 01:17:43 +0900

    feat: login 페이지 ui 구현

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -6,20 +6,22 @@ export default function LoginPage() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen items-center justify-center bg-gray-100">
-        <section className="flex w-[74.4rem] flex-col items-center gap-[1.2rem] border border-gray-400 bg-white px-[22.2rem] py-[12.7rem]">
-          <h1 className="mb-[1.2rem] text-[2.4rem] font-bold text-pink-500">
+      <main className="flex items-center justify-center bg-gray-100">
+        <section className="my-[6.9rem] flex w-[74.4rem] flex-col items-center border border-gray-400 bg-white px-[22.2rem] py-[12.7rem]">
+          <h1 className="jp-head3 mb-[1.4rem] font-bold text-pink-500">
             会員ログイン
           </h1>
-          <p className="mb-[2.4rem] text-[1.6rem] text-gray-700">
+          <p className="jp-body2 text-gray-700">
             LINEアカウントですぐにログイン
           </p>
           <button
             type="button"
-            className="mt-[1.2rem] flex h-[4.8rem] w-[30rem] cursor-pointer items-center justify-center gap-[2.1rem] rounded-[0.6rem] bg-[#06C755] font-bold text-white"
+            className="jp-body1 relative mt-[12rem] flex h-[4.8rem] cursor-pointer items-center rounded-[0.6rem] bg-[#06C755] pl-[5.4rem] pr-[5.4rem] font-bold text-white"
           >
-            <SvgLine></SvgLine>
-            LINEでログイン
+            <span className="absolute left-[2.1rem] flex items-center">
+              <SvgLine size={20} />
+            </span>
+            <span className="w-[18.4rem] justify-center">LINEでログイン</span>
           </button>
         </section>
       </main>
