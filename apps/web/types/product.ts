7b2943ea0d8e6ce5e30b1d4dd3aa8a7844b1fcf1
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-09 21:23:17 +0900

    fix: merge

--- apps/web/types/product.ts
+++ apps/web/types/product.ts
@@ -1,14 +1,11 @@
-export interface ProductItems {
-  rank?: number;
-  brand: string;
-  title: string;
-  description: string;
+export interface ProductItem {
+  ranking?: number;
+  brandName: string;
+  productName: string;
+  unit: string;
   productId: number;
   isLiked: boolean;
   rating: number;
   reviewCount: number;
-  likeCount: number;
   imageUrl?: string;
-  handleLikeToggle?: (productId: number, isLiked: boolean) => void;
-  handleCardClick?: (productId: number) => void;
 }
