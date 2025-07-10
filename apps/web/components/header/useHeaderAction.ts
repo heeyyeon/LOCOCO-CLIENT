7831d4e116745f2db738b83f5e7e675f1de2f541
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-10 18:12:43 +0900

    refactor: header 핸들러 props 타입 수정

--- apps/web/components/header/useHeaderAction.ts
+++ apps/web/components/header/useHeaderAction.ts
@@ -1,40 +1,48 @@
 import { CATEGORY_OPTIONS, CATEGORY_NAME } from 'constants/category';
-import { CategoryName, CategoryOption } from 'types/category';
+import { CategoryKey, CategoryName, CategoryOptionEng } from 'types/category';
 import { useMemo, useState } from 'react';
+import useCustomSearchParams from './useCustomSearchParams';
 
 export function useHeaderAction() {
+  const { setSearchParams, resetSearchParams } = useCustomSearchParams();
+
   const [selectedCategory, setSelectedCategory] = useState<CategoryName | null>(
     null
   );
-  const [selectedOption, setSelectedOption] = useState<CategoryOption | null>(
-    null
-  );
+  const [selectedOption, setSelectedOption] =
+    useState<CategoryOptionEng | null>(null);
   const [isSearching, setIsSearching] = useState(false);
   const [searchValue, setSearchValue] = useState('');
 
-  const categories = useMemo(
-    () =>
-      Object.entries(CATEGORY_OPTIONS).map(([key, option]) => ({
-        category: CATEGORY_NAME[key as keyof typeof CATEGORY_NAME],
-        options: Object.values(option) as CategoryOption[],
-      })),
-    []
-  );
+  const categories = useMemo(() => {
+    return (Object.keys(CATEGORY_OPTIONS) as CategoryKey[]).map((key) => ({
+      key,
+      name: CATEGORY_NAME[key],
+      options: Object.keys(CATEGORY_OPTIONS[key]) as CategoryOptionEng[],
+    }));
+  }, []);
 
-  const handleSelectCategory = (title: CategoryName) => {
-    setSelectedCategory(title);
+  const handleSelectCategory = (key: CategoryKey) => {
+    setSelectedCategory(CATEGORY_NAME[key]);
     setSelectedOption(null);
     setIsSearching(false);
+    resetSearchParams();
+
+    setSearchParams({ midCategory: key });
   };
 
-  const handleSelectOption = (option: CategoryOption) => {
+  const handleSelectOption = (option: CategoryOptionEng) => {
     setSelectedOption(option);
+
+    setSearchParams({ subCategory: option });
   };
 
   const handleOpenSearchBar = () => {
     setIsSearching((prev) => !prev);
     setSelectedCategory(null);
     setSelectedOption(null);
+
+    resetSearchParams();
   };
 
   const handleChangeSearch = (text: string) => {
@@ -42,7 +50,7 @@ export function useHeaderAction() {
   };
 
   const activeMenu = useMemo(
-    () => categories.find((cat) => cat.category === selectedCategory),
+    () => categories.find((category) => category.name === selectedCategory),
     [categories, selectedCategory]
   );
 
