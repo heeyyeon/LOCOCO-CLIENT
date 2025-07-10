edf1c9b47f1362f035cfd29c6ea5b26656e39085
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-10 18:09:12 +0900

    refactor: 파라미터 다루는 커스텀 훅 추가

---
+++ apps/web/components/header/useCustomSearchParams.ts
@@ -0,0 +1,34 @@
+'use client';
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+type NewParamsType = { [key: string]: string };
+
+export default function useCustomSearchParams() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const _searchParams = useSearchParams();
+  let searchParams = new URLSearchParams(_searchParams.toString());
+
+  const setNewParams = (newParams: NewParamsType) => {
+    for (const [key, value] of Object.entries(newParams)) {
+      if (value) searchParams.set(key, value);
+      else searchParams.delete(key);
+    }
+    return searchParams.toString();
+  };
+
+  const setSearchParams = (newParams: NewParamsType) => {
+    return router.push(`${pathname}?${setNewParams(newParams)}`);
+  };
+
+  const resetSearchParams = () => {
+    searchParams = new URLSearchParams();
+    return router.push(pathname);
+  };
+
+  return {
+    setSearchParams,
+    resetSearchParams,
+  };
+}
