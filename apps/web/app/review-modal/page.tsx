8daaa2278537ed62758e9fc95386aca6d5708e93
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-07-11 17:15:28 +0900

    feat: 리뷰 조회 page 파일 생성(더미 값 추가)

---
+++ apps/web/app/review-modal/page.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import 'swiper/css';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import ReviewModalLayout from './components/ReviewModalLayout';
+
+const reviewList = [
+  {
+    mediaList: [
+      {
+        type: 'video' as const,
+        url: 'https://www.w3schools.com/html/mov_bbb.mp4',
+      },
+    ],
+    user: { name: '김정은', avatarUrl: '' },
+    date: '2025年07月01日',
+    likeCount: 123,
+  },
+  {
+    mediaList: [
+      {
+        type: 'image' as const,
+        url: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb',
+      },
+    ],
+    user: { name: '권동희', avatarUrl: '' },
+    date: '2025年07月01日',
+    likeCount: 45,
+  },
+  {
+    mediaList: [
+      {
+        type: 'image' as const,
+        url: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb',
+      },
+    ],
+    user: { name: '정희연', avatarUrl: '' },
+    date: '2025年07月01日',
+    likeCount: 454,
+  },
+];
+
+export default function Page() {
+  return (
+    <Swiper
+      direction="vertical"
+      slidesPerView="auto"
+      centeredSlides
+      spaceBetween={1}
+      modules={[Pagination]}
+      className="inset-0 h-screen w-screen bg-black/70"
+    >
+      {reviewList.map((review, idx) => (
+        <SwiperSlide
+          key={idx}
+          className="flex h-[55.2rem] items-center justify-center"
+        >
+          <ReviewModalLayout
+            mediaList={review.mediaList}
+            user={review.user}
+            date={review.date}
+            likeCount={review.likeCount}
+            onClose={() => {}}
+          >
+            {/* 우측 리뷰 내용 등 */}
+            <div>리뷰 내용</div>
+          </ReviewModalLayout>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
