890a344e2a4288c0d6664d3554c2f0a87555148f
kojesung <js95112345@gmail.com>
2025-07-14 02:14:59 +0900

    delete: 테스트용 코드 삭제

--- apps/web/app/(with-layout)/page.tsx
+++ apps/web/app/(with-layout)/page.tsx
@@ -1,45 +1,11 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
-import { ApiResponseListVideoResponse } from 'api/data-contracts';
-import { apiRequest } from 'app/api/apiRequest';
-import { getAccessToken } from 'app/api/token';
 import { SvgJapaneseReview, SvgKoreanReview } from '@/icons';
 import HomeSection from '../home/components/home';
 import HomeBanner from '../home/components/home-banner';
 import HomeUpdateDate from '../home/components/home-update-date';
 
 export default function Main() {
-  const { data, error, isLoading, isError, refetch } = useQuery({
-    queryKey: ['youtube-trends'],
-    queryFn: async () => {
-      return await apiRequest<ApiResponseListVideoResponse>({
-        endPoint: 'api/youtube/trends',
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${getAccessToken()}`,
-        },
-      });
-    },
-    retry: 1,
-    staleTime: 5 * 60 * 1000,
-    gcTime: 10 * 60 * 1000,
-  });
-  console.log(data?.data);
-  if (isLoading) {
-    return <div>로딩 중</div>;
-  }
-
-  if (isError) {
-    console.error('API 에러:', error);
-    return (
-      <div>
-        <p>에러 발생: {error?.message}</p>
-        <button onClick={() => refetch()}>다시 시도</button>
-      </div>
-    );
-  }
-
   return (
     <main className="flex w-full flex-col">
       <HomeBanner />
