dddf3549b63b9b1310231c00d08ed1cb4787b228
ljh0608 <m98m@naver.com>
2025-07-15 04:00:06 +0900

    chore: Next Image ** hostname 추가

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -14,6 +14,14 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
     ],
   },
 };
