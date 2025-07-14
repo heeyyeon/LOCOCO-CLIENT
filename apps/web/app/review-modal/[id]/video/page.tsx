1f7cb69655b7f840bd821bd296045e56f7fdf77c
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-07-15 06:56:56 +0900

    feat: 비디오 page.tsx 생성

---
+++ apps/web/app/review-modal/[id]/video/page.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useRouter } from 'next/navigation';
+import ReviewModalLayout from '../../components/review-modal-layout';
+import { videoReviewMocks } from '../../mocks';
+
+export default function Page() {
+  const router = useRouter();
+
+  return (
+    <Swiper
+      direction="vertical"
+      slidesPerView={1.2}
+      centeredSlides
+      spaceBetween={-45}
+      className="inset-0 h-screen w-screen bg-black/70"
+    >
+      {videoReviewMocks.map((review, idx) => (
+        <SwiperSlide
+          key={idx}
+          className="flex h-[55.2rem] items-center justify-center"
+        >
+          <ReviewModalLayout
+            id={idx}
+            mediaList={review.mediaList}
+            user={{
+              name: review.authorName,
+              avatarUrl: review.profileImageUrl || undefined,
+              uploadAt: review.writtenTime,
+            }}
+            likeCount={review.likeCount}
+            brandName={review.brandName}
+            productName={review.productName}
+            productOption={review.option}
+            rating={review.rating}
+            isReceipt={review.receiptUploaded}
+            positiveComment={review.positiveComment}
+            negativeComment={review.negativeComment}
+            productImageUrl={review.productImageUrl}
+            onClose={() => router.back()}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
