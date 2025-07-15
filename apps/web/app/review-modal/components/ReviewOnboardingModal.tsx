3d439877fe298b497383bf535c1c081c6cd46ddd
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-15 23:31:49 +0900

    style: vector 삭제

--- apps/web/app/review-modal/components/ReviewOnboardingModal.tsx
+++ apps/web/app/review-modal/components/ReviewOnboardingModal.tsx
@@ -1,67 +1,65 @@
-import { SvgClose, SvgVector, SvgVector470, SvgVector469 } from '@/icons';
+import { SvgClose, SvgVector } from '@/icons';
 
-export default function ReviewOnboarding() {
+function LeftAnimation() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80">
-      <button className="absolute right-0 top-0 z-10 cursor-pointer p-[1.4rem]">
-        <SvgClose width={36} height={36} className="text-gray-500" />
-      </button>
-
-      <div className="relative flex w-[93.6rem] flex-col px-[22.4rem] py-[11.8rem]">
-        {/* Main content */}
-        <div className="flex w-[48.8rem] flex-col items-center gap-[4rem] self-stretch">
-          <div className="flex items-center gap-[3.2rem]">
-            {/* left */}
-            <div className="relative flex h-[22.8rem] w-[22.8rem] flex-col items-center justify-center rounded-[1.6rem] bg-[rgba(235,236,239,0.20)]">
-              <div className="absolute top-[2rem] h-[9.9704rem] w-[2.6408rem]">
-                <SvgVector470
-                  className="animate-vector470-move h-[9.9704rem] w-[2.6408rem] shrink-0 opacity-80"
-                  stroke="rgba(255,72,143,0.80)"
-                  strokeWidth={26.408}
-                />
-                <div className="animate-vector470-move absolute left-1/2 top-[4rem] aspect-[26.41/26.41] h-[2.6408rem] w-[2.6408rem] shrink-0 -translate-x-1/2 transform rounded-full bg-pink-500"></div>
-              </div>
-              <div className="animate-vector470-move absolute top-[7rem] flex w-[21rem] items-center justify-center">
-                <SvgVector
-                  className="ml-[1.3204rem] h-[6.5175rem] w-[7.3456rem] text-white"
-                  fill="white"
-                />
-              </div>
-            </div>
-            {/* right */}
-            <div className="relative flex h-[22.8rem] w-[22.8rem] items-center justify-center rounded-[1.6rem] bg-[rgba(235,236,239,0.20)] px-0 py-[5.2464rem] backdrop-blur-sm">
-              <div className="h-[12.3072rem] w-[6.7341rem] shrink-0 rounded-[13.9326rem] bg-white"></div>
-              <div className="animate-vector469-move absolute left-1/2 top-[7rem] h-[1.3933rem] w-[1.3933rem] shrink-0 -translate-x-1/2 transform rounded-full bg-pink-500"></div>
-              <div className="absolute top-[7rem] flex items-center justify-center">
-                <SvgVector469 className="h-[3.6015rem]" />
-              </div>
-            </div>
-          </div>
-          <div className="text-center text-lg font-medium text-white">
-            タッチパッドを<p className="inline text-pink-500">スワイプ</p>
-            するか、マウスを
-            <p className="inline text-pink-500"> スクロール</p>
-            してより多くのビデオレビューをご覧ください
-          </div>
-        </div>
+    <div className="relative flex h-[22.8rem] w-[22.8rem] flex-col items-center justify-center rounded-[1.6rem] bg-[rgba(235,236,239,0.20)]">
+      <div className="animate-vector470-move absolute left-1/2 top-[2rem] h-0 w-[2.6408rem] -translate-x-1/2 rounded-[1.3204rem] bg-gradient-to-b from-transparent to-pink-500 opacity-80"></div>
+      <div className="animate-circle-move absolute left-1/2 top-[2rem] h-[2.6408rem] w-[2.6408rem] -translate-x-1/2 rounded-full bg-pink-500"></div>
+      <div className="animate-vector-move absolute flex items-center justify-center">
+        <SvgVector
+          className="ml-[1.3204rem] h-[6.5175rem] w-[7.3456rem] text-white"
+          fill="white"
+        />
       </div>
-
       <style jsx>{`
         @keyframes vector470Move {
           0% {
-            transform: translateY(0);
+            height: 0;
           }
           50% {
-            transform: translateY(5rem);
+            height: 8rem;
           }
           100% {
-            transform: translateY(-3rem);
+            height: 0;
           }
         }
 
         .animate-vector470-move {
-          animation: vector470Move 3s ease-out forwards;
-          will-change: transform;
+          animation: vector470Move 2s ease-out forwards;
+          will-change: height;
+        }
+        @keyframes circleMove {
+          0% {
+            top: 2rem;
+          }
+          50% {
+            top: 10rem;
+          }
+          100% {
+            top: 2rem;
+          }
+        }
+
+        .animate-circle-move {
+          animation: circleMove 2s ease-out forwards;
+          will-change: top;
+        }
+
+        @keyframes vectorMove {
+          0% {
+            top: 2rem;
+          }
+          50% {
+            top: 8rem;
+          }
+          100% {
+            top: 2rem;
+          }
+        }
+
+        .animate-vector-move {
+          animation: vectorMove 2s ease-out forwards;
+          will-change: top;
         }
 
         @keyframes vector469Move {
@@ -75,12 +73,45 @@ export default function ReviewOnboarding() {
             transform: translateY(0);
           }
         }
-
-        .animate-vector469-move {
-          animation: vector469Move 3s ease-out forwards;
-          will-change: transform;
-        }
       `}</style>
+    </div>
+  );
+}
+
+function RightAnimation() {
+  return (
+    <div className="relative flex h-[22.8rem] w-[22.8rem] items-center justify-center rounded-[1.6rem] bg-[rgba(235,236,239,0.20)] px-0 py-[5.2464rem] backdrop-blur-sm">
+      <div className="h-[12.3072rem] w-[6.7341rem] rounded-[13.9326rem] bg-white"></div>
+      <div className="animate-vector469-move absolute left-1/2 top-[7rem] h-[1.3933rem] w-[1.3933rem] -translate-x-1/2 rounded-full bg-pink-500"></div>
+      <div className="absolute top-[7rem] flex items-center justify-center">
+        <div className="h-[3.6015rem] w-[1.3933rem] rounded-full bg-gradient-to-b from-transparent to-pink-500 opacity-80"></div>
+      </div>
+    </div>
+  );
+}
+
+export default function ReviewOnboardingModal() {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80">
+      <button className="absolute right-0 top-0 z-10 cursor-pointer p-[1.4rem]">
+        <SvgClose width={36} height={36} className="text-gray-500" />
+      </button>
+
+      <div className="relative flex w-[93.6rem] flex-col px-[22.4rem] py-[11.8rem]">
+        {/* Main content */}
+        <div className="flex w-[48.8rem] flex-col items-center gap-[4rem] self-stretch">
+          <div className="flex items-center gap-[3.2rem]">
+            <LeftAnimation />
+            <RightAnimation />
+          </div>
+          <div className="text-center text-lg font-medium text-white">
+            タッチパッドを<p className="inline text-pink-500">スワイプ</p>
+            するか、マウスを
+            <p className="inline text-pink-500"> スクロール</p>
+            してより多くのビデオレビューをご覧ください
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
