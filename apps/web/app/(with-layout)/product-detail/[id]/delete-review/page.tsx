e8ac586f9bf503bfb1b9ef8eca1d1151b7dbdf05
kojesung <js95112345@gmail.com>
2025-07-15 20:37:32 +0900

    fix: 하드 네비게이션 시 홈으로

--- apps/web/app/(with-layout)/product-detail/[id]/delete-review/page.tsx
+++ apps/web/app/(with-layout)/product-detail/[id]/delete-review/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from 'next/navigation';
 
-export default async function DeleteReviewPage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-
-  redirect(`/product-detail/${id}`);
+export default async function DeleteReviewPage() {
+  redirect('/');
 }
