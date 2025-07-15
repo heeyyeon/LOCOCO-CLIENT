74019d312f820b16b8af227666525503f1a3228f
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-07-15 16:00:43 +0900

    refactor: page.tsx 중복 코드(비디오/이미지) 별도 컴포넌트 파일로 분리

--- apps/web/app/review-modal/[reviewId]/video/page.tsx
+++ apps/web/app/review-modal/[reviewId]/video/page.tsx
@@ -1,48 +1,16 @@
 'use client';
 
-import 'swiper/css';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import { useRouter } from 'next/navigation';
-import ReviewModalLayout from '../../components/review-modal-layout';
+import ReviewModalSwiper from '../../components/review-modal-swiper';
 import { videoReviewMocks } from '../../mocks';
 
 export default function Page() {
   const router = useRouter();
 
   return (
-    <Swiper
-      direction="vertical"
-      slidesPerView={1.2}
-      centeredSlides
-      spaceBetween={-45}
-      className="inset-0 h-screen w-screen bg-black/70"
-    >
-      {videoReviewMocks.map((review) => (
-        <SwiperSlide
-          key={review.reviewId}
-          className="flex h-[55.2rem] items-center justify-center"
-        >
-          <ReviewModalLayout
-            id={review.reviewId}
-            mediaList={review.mediaList}
-            user={{
-              name: review.authorName,
-              avatarUrl: review.profileImageUrl,
-              uploadAt: review.writtenTime,
-            }}
-            likeCount={review.likeCount}
-            brandName={review.brandName}
-            productName={review.productName}
-            productOption={review.option}
-            rating={review.rating}
-            isReceipt={review.receiptUploaded}
-            positiveComment={review.positiveComment}
-            negativeComment={review.negativeComment}
-            productImageUrl={review.productImageUrl}
-            onClose={() => router.back()}
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <ReviewModalSwiper
+      reviews={videoReviewMocks}
+      onClose={() => router.back()}
+    />
   );
 }
