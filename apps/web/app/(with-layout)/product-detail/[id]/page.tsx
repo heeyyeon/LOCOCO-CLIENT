2d8bdfe7fc06d75443bfcf276a32a691f6f28f0f
kojesung <js95112345@gmail.com>
2025-07-15 20:40:53 +0900

    delete: 모달 확인용 코드 삭제

--- apps/web/app/(with-layout)/product-detail/[id]/page.tsx
+++ apps/web/app/(with-layout)/product-detail/[id]/page.tsx
@@ -1,20 +1,3 @@
-import Link from 'next/link';
-
-export default async function ProductDetailPage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  const reviewId = 5;
-
-  return (
-    <div>
-      <h1>상품 상세 페이지 - ID: {id}</h1>
-
-      <Link href={`/product-detail/${id}/delete-review?reviewId=${reviewId}`}>
-        리뷰 삭제
-      </Link>
-    </div>
-  );
+export default async function ProductDetailPage() {
+  return <div></div>;
 }
