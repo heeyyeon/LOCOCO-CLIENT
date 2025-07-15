72f4cfeb2d8485589b031b774406e4a44963df37
kojesung <js95112345@gmail.com>
2025-07-16 05:53:48 +0900

    feat: cookie로 로그인 상태 접근 후 좋아요 기능 핸들링

--- apps/web/components/card/hooks/use-product-like.ts
+++ apps/web/components/card/hooks/use-product-like.ts
@@ -2,12 +2,26 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { PRODUCT_QUERIES } from 'app/(with-layout)/(home)/components/home-section-product';
 import { apiRequest } from 'app/api/apiRequest';
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 interface UseProductLikeProps {
   initialIsLiked: boolean;
 }
 
 export function useProductLike({ initialIsLiked }: UseProductLikeProps) {
+  const router = useRouter();
+  function getCookie(name: string): string | null {
+    if (typeof document === 'undefined') return null;
+
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+      return parts.pop()?.split(';').shift() || null;
+    }
+    return null;
+  }
+  const userToken = getCookie('AccessToken');
+  console.log(userToken);
   const [isLiked, setIsLiked] = useState(initialIsLiked);
   const queryClient = useQueryClient();
 
@@ -36,13 +50,22 @@ export function useProductLike({ initialIsLiked }: UseProductLikeProps) {
     },
   });
 
-  const handleLikeClick = async (e: React.MouseEvent, productId: number) => {
+  const handleLikeClick = async (
+    e: React.MouseEvent,
+    productId: number,
+    userToken?: string | null
+  ) => {
     e.stopPropagation();
+    if (!userToken) {
+      router.push('/login');
+      return;
+    }
     likeMutation.mutate(productId);
   };
 
   return {
     isLiked,
     handleLikeClick,
+    userToken,
   };
 }
