fd659584ad9406dcd8134916e6fa99bc717668af
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-07-16 23:04:58 +0900

    feat: swiper 속성 수정

--- apps/web/app/review-modal/components/review-modal-swiper.tsx
+++ apps/web/app/review-modal/components/review-modal-swiper.tsx
@@ -19,7 +19,7 @@ export default function ReviewModalSwiper({
       direction="vertical"
       slidesPerView={1.2}
       centeredSlides
-      spaceBetween={-75}
+      spaceBetween={-45}
       className="inset-0 h-screen w-screen bg-black/70"
     >
       {reviews.map((review) => (
