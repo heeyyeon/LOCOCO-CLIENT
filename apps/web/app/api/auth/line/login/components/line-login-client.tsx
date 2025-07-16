b3edd3866ce95c6cff4969993c748f3df62b2645
ljh0608 <m98m@naver.com>
2025-07-17 00:09:30 +0900

    refactor: line login redirect uri queryParams 추가

--- apps/web/app/api/auth/line/login/components/line-login-client.tsx
+++ apps/web/app/api/auth/line/login/components/line-login-client.tsx
@@ -12,10 +12,16 @@ export default function LineLoginClient() {
 
   const code = searchParams.get('code') || '';
   const state = searchParams.get('state') || '';
+  const lineLoginRedirectURL =
+    process.env.NEXT_PUBLIC_LINE_LOGIN_REDIRECT_URL || '';
 
   const { isSuccess } = useQuery({
-    queryKey: LINE_LOGIN_QUERY_KEYS.LINE_LOGIN(code, state),
-    queryFn: () => lineLogin({ code, state }),
+    queryKey: LINE_LOGIN_QUERY_KEYS.LINE_LOGIN(
+      code,
+      state,
+      lineLoginRedirectURL
+    ),
+    queryFn: () => lineLogin({ code, state, lineLoginRedirectURL }),
   });
 
   useEffect(() => {
