5b9871f25899ab26c4ad8038d26a692ce67e8705
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-16 16:10:25 +0900

    feat: loading 상태 text 중앙정렬

--- apps/web/app/(with-layout)/search/page.client.tsx
+++ apps/web/app/(with-layout)/search/page.client.tsx
@@ -246,10 +246,20 @@ export default function SearchPageClient() {
         selectedTab={selectedTab}
         handleClickTab={handleClickTab}
       />
-      {!isClient && <div>Loading...</div>}
-      {isClient && isLoading && <div>데이터를 불러오는 중...</div>}
+      {!isClient && (
+        <div className="mx-auto flex h-screen w-[1366px] items-center justify-center">
+          Loading...
+        </div>
+      )}
+      {isClient && isLoading && (
+        <div className="mx-auto flex h-screen w-[1366px] items-center justify-center">
+          데이터를 불러오는 중...
+        </div>
+      )}
       {isClient && hasError && (
-        <div>데이터를 불러오는 중 오류가 발생했습니다.</div>
+        <div className="mx-auto flex h-screen w-[1366px] items-center justify-center">
+          데이터를 불러오는 중 오류가 발생했습니다.
+        </div>
       )}
       {isClient && !isLoading && !hasError && <>{tabRender}</>}
     </div>
