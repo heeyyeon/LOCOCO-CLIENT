540462ef311ec0b3d8c9d008c2af19dc5527a391
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-07-18 05:01:43 +0900

    feat: 미우스 휠로 스와이프 기능 추가

--- apps/web/app/review-modal/components/review-modal-swiper.tsx
+++ apps/web/app/review-modal/components/review-modal-swiper.tsx
@@ -2,6 +2,7 @@
 
 import SwiperCore from 'swiper';
 import 'swiper/css';
+import { Mousewheel, FreeMode } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useEffect, useState } from 'react';
 import type { ReviewDetail } from '../types';
@@ -33,6 +34,19 @@ export default function ReviewModalSwiper({
       slidesPerView={1.2}
       centeredSlides
       spaceBetween={-45}
+      modules={[Mousewheel, FreeMode]}
+      mousewheel={{
+        sensitivity: 0.8,
+        releaseOnEdges: true,
+        forceToAxis: true,
+      }}
+      freeMode={{
+        enabled: true,
+        sticky: true,
+        momentum: false,
+      }}
+      speed={400}
+      resistance={false}
       className="inset-0 h-screen w-screen bg-black/70"
     >
       {reviews.map((review) => (
