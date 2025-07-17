036c19a52b1900141fe8cb41231170cb0e2417a1
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-07-17 18:23:04 +0900

    feat: video 파일 추가

---
+++ apps/web/app/review-modal/hooks/review-api.ts
@@ -0,0 +1,114 @@
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  useQueries,
+} from '@tanstack/react-query';
+import { apiRequest } from 'app/api/apiRequest';
+import {
+  ApiResponseImageReviewDetailResponse,
+  ApiResponseMainImageReviewResponse,
+  ApiResponseMainVideoReviewResponse,
+  ApiResponseVideoReviewDetailResponse,
+} from '../../../api/data-contracts';
+import { REVIEW_KEYS } from '../../../constants/query-key';
+
+export const useImageReviews = (productId?: number) => {
+  return useQuery({
+    queryKey: productId
+      ? REVIEW_KEYS.IMAGE_DETAIL(productId)
+      : REVIEW_KEYS.IMAGE_LISTS(),
+    queryFn: async (): Promise<ApiResponseMainImageReviewResponse> => {
+      if (productId) {
+        return apiRequest({
+          endPoint: `/api/reviews/details/image`,
+          method: 'GET',
+          params: {
+            productId: productId.toString(),
+          },
+        });
+      }
+      return apiRequest({
+        endPoint: '/api/reviews/image',
+        method: 'GET',
+      });
+    },
+  });
+};
+
+export const useAllImageReviewDetails = (
+  reviews: { reviewId: number }[] | undefined
+) => {
+  return useQueries({
+    queries: (reviews || []).map((review) => ({
+      queryKey: REVIEW_KEYS.IMAGE_DETAIL(review.reviewId),
+      queryFn: async (): Promise<ApiResponseImageReviewDetailResponse> => {
+        return apiRequest({
+          endPoint: `/api/reviews/details/${review.reviewId}/image`,
+          method: 'GET',
+        });
+      },
+      enabled: !!reviews && reviews.length > 0,
+    })),
+  });
+};
+
+export const useVideoReviews = (productId?: number) => {
+  return useQuery({
+    queryKey: productId
+      ? REVIEW_KEYS.VIDEO_DETAIL(productId)
+      : REVIEW_KEYS.VIDEO_LISTS(),
+    queryFn: async (): Promise<ApiResponseMainVideoReviewResponse> => {
+      if (productId) {
+        return apiRequest({
+          endPoint: `/api/reviews/details/video`,
+          method: 'GET',
+          params: {
+            productId: productId.toString(),
+          },
+        });
+      }
+      return apiRequest({
+        endPoint: '/api/reviews/video',
+        method: 'GET',
+      });
+    },
+  });
+};
+
+export const useAllVideoReviewDetails = (
+  reviews: { reviewId: number }[] | undefined
+) => {
+  return useQueries({
+    queries: (reviews || []).map((review) => ({
+      queryKey: REVIEW_KEYS.VIDEO_DETAIL(review.reviewId),
+      queryFn: async (): Promise<ApiResponseVideoReviewDetailResponse> => {
+        return apiRequest({
+          endPoint: `/api/reviews/details/${review.reviewId}/video`,
+          method: 'GET',
+        });
+      },
+      enabled: !!reviews && reviews.length > 0,
+    })),
+  });
+};
+
+export const useReviewLikeToggle = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (reviewId: number) => {
+      return apiRequest({
+        endPoint: `/api/likes/reviews/${reviewId}`,
+        method: 'POST',
+      });
+    },
+    onSuccess: (_, reviewId) => {
+      if (typeof reviewId === 'number') {
+        queryClient.invalidateQueries({
+          queryKey: REVIEW_KEYS.IMAGE_DETAIL(reviewId),
+        });
+      }
+    },
+  });
+};
