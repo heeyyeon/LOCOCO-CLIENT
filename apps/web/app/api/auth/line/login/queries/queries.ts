980306de99509471e2535fa5a8c74c73279a1eca
ljh0608 <m98m@naver.com>
2025-07-18 03:24:31 +0900

    fix: remove login api query string

--- apps/web/app/api/auth/line/login/queries/queries.ts
+++ apps/web/app/api/auth/line/login/queries/queries.ts
@@ -1,5 +1,5 @@
 export const LINE_LOGIN_QUERY_KEYS = {
   ALL: ['LINE_LOGIN'] as const,
-  LINE_LOGIN: (code: string, state: string, lineLoginRedirectURL: string) =>
-    [...LINE_LOGIN_QUERY_KEYS.ALL, code, state, lineLoginRedirectURL] as const,
+  LINE_LOGIN: (code: string, state: string) =>
+    [...LINE_LOGIN_QUERY_KEYS.ALL, code, state] as const,
 };
