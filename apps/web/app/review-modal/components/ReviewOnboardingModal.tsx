fb294386e1984a608867abc4c8916859396f7d8e
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-18 14:07:58 +0900

    feat: onboarding 구현 완료

--- apps/web/app/review-modal/components/ReviewOnboardingModal.tsx
+++ apps/web/app/review-modal/components/ReviewOnboardingModal.tsx
@@ -1,11 +1,13 @@
-import { SvgClose, SvgVector, SvgVector470 } from '@/icons';
+'use client';
+
+import { SvgClose, SvgVector, SvgVector469, SvgVector470 } from '@/icons';
 
 function LeftAnimation() {
   return (
     <div className="relative flex h-[22.8rem] w-[22.8rem] rounded-[1.6rem] bg-[rgba(235,236,239,0.20)]">
       <div className="absolute left-[8.87rem] top-[2rem]">
         <SvgVector470 height={99.704} width={26.408} />
-        <div className="absolute bottom-[0rem] left-1/2 h-[2.6408rem] w-[2.6408rem] -translate-x-1/2 rounded-full bg-pink-500"></div>
+        <div className="absolute bottom-[0rem] left-1/2 h-[2rem] w-[2rem] -translate-x-1/2 rounded-full bg-pink-500"></div>
       </div>
       <div className="absolute left-[7.2rem] top-[10rem]">
         <SvgVector width={73.456} height={65.175} fill="white" />
@@ -17,15 +19,26 @@ function LeftAnimation() {
 function RightAnimation() {
   return (
     <div className="relative flex h-[22.8rem] w-[22.8rem] items-center justify-center rounded-[1.6rem] bg-[rgba(235,236,239,0.20)] px-0 py-[5.2464rem] backdrop-blur-sm">
-      <div className="h-[12.3072rem] w-[6.7341rem] rounded-[13.9326rem] bg-white"></div>
+      <div className="relative h-[12.3072rem] w-[6.7341rem] rounded-[13.9326rem] bg-white">
+        <div className="absolute left-1/2 top-[1.3rem] -translate-x-1/2">
+          <SvgVector469 height={36.015} />
+          <div className="absolute bottom-[0rem] left-1/2 h-[1rem] w-[1rem] -translate-x-1/2 rounded-full bg-pink-500"></div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default function ReviewOnboardingModal() {
+  const handleClose = () => {
+    console.log('close');
+  };
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80">
-      <button className="absolute right-0 top-0 z-10 cursor-pointer p-[1.4rem]">
+      <button
+        className="absolute right-0 top-0 z-10 cursor-pointer p-[1.4rem]"
+        onClick={handleClose}
+      >
         <SvgClose width={36} height={36} className="text-gray-500" />
       </button>
 
