4718ef825e343320c23fe2abb31bc693e692733c
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-08-08 16:07:24 +0900

    refactor: 불필요한 import 제거

--- apps/web/app/review-modal/components/modal-structure/review-modal-layout.tsx
+++ apps/web/app/review-modal/components/modal-structure/review-modal-layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { OverlayProvider } from 'overlay-kit';
-
 import { MediaItem } from '../../types';
 import { MediaInfo, MediaViewer } from '../media';
 import { ReviewInfo } from '../review';
@@ -52,40 +50,38 @@ export default function ReviewModallayout({
   children,
 }: ReviewModallayoutProps) {
   return (
-    <OverlayProvider>
-      <div className="relative flex h-full w-full items-center justify-center">
-        <div className="relative flex h-[55.2rem] rounded-xl bg-white">
-          {/* 좌측: 미디어 + 하단정보 */}
-          <div className="relative flex w-[55.2rem] flex-col">
-            <MediaViewer mediaList={mediaList} />
-            <MediaInfo
-              userStatus={userStatus}
-              reviewId={id}
-              user={user}
-              date={user.uploadAt}
-              likeCount={likeCount}
-              isLiked={isLiked}
-            />
-          </div>
-          {/* 우측: 리뷰 상세 */}
-          <div className="relative flex w-[38.4rem] flex-col">
-            <ReviewInfo
-              reviewId={id}
-              productId={productId}
-              brandName={brandName}
-              productName={productName}
-              option={productOption}
-              rating={rating}
-              positiveComment={positiveComment}
-              negativeComment={negativeComment}
-              productImageUrl={productImageUrl}
-              receiptUploaded={isReceipt || false}
-              onClose={onClose}
-            />
-          </div>
+    <div className="relative flex h-full w-full items-center justify-center">
+      <div className="relative flex h-[55.2rem] rounded-xl bg-white">
+        {/* 좌측: 미디어 + 하단정보 */}
+        <div className="relative flex w-[55.2rem] flex-col">
+          <MediaViewer mediaList={mediaList} />
+          <MediaInfo
+            userStatus={userStatus}
+            reviewId={id}
+            user={user}
+            date={user.uploadAt}
+            likeCount={likeCount}
+            isLiked={isLiked}
+          />
         </div>
-        {children}
+        {/* 우측: 리뷰 상세 */}
+        <div className="relative flex w-[38.4rem] flex-col">
+          <ReviewInfo
+            reviewId={id}
+            productId={productId}
+            brandName={brandName}
+            productName={productName}
+            option={productOption}
+            rating={rating}
+            positiveComment={positiveComment}
+            negativeComment={negativeComment}
+            productImageUrl={productImageUrl}
+            receiptUploaded={isReceipt || false}
+            onClose={onClose}
+          />
+        </div>
       </div>
-    </OverlayProvider>
+      {children}
+    </div>
   );
 }
