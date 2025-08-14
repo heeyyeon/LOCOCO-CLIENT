cf421fc8f777585d809d7af557e45f8163d20a77
kojesung <js95112345@gmail.com>
2025-08-14 16:32:33 +0900

    feat: getYoutubeVideo 함수 유틸로 분리

--- apps/web/app/(with-layout)/(home)/components/server-wrapper/home-youtube-server.tsx
+++ apps/web/app/(with-layout)/(home)/components/server-wrapper/home-youtube-server.tsx
@@ -1,42 +1,12 @@
 import React from 'react';
 
-import { ApiResponseListTrendsYoutubeResponse } from '@typescript-swagger/data-contracts';
-import { apiRequest } from 'app/api/apiRequest';
-import { validateYoutubeVideoList } from 'utils/youtube';
-
 import { SvgKoreanReview } from '@lococo/icons';
 
+import { getYoutubeVideo } from '../../utils/getYoutubeVideos';
 import HomeSection from '../home-section';
 
-interface VideoWithAllProps {
-  url: string;
-  id: number;
-  topic: string;
-  title: string;
-  popularity: number;
-  viewCount: number;
-  uploadedAt: string;
-}
-
 export default async function HomeYoutubeServer() {
-  const response = await apiRequest<ApiResponseListTrendsYoutubeResponse>({
-    endPoint: '/api/youtube/trends',
-  });
-
-  const videos = response.data;
-
-  if (!videos || videos.length === 0) {
-    return <div>영상이 없습니다</div>;
-  }
-
-  const videoUrls = videos.map((video) => video.url || '').filter((url) => url);
-  const validatedVideoUrls = await validateYoutubeVideoList(videoUrls, 25);
-  const showVideos: VideoWithAllProps[] = videos
-    .map((video, index) => ({
-      ...video,
-      url: validatedVideoUrls[index] || video.url,
-    }))
-    .slice(0, 6);
+  const { showVideos } = await getYoutubeVideo();
   return (
     <HomeSection>
       <HomeSection.Header>
