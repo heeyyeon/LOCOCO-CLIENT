259bbfff64faca4b97449885bbcf837e46ed5164
kojesung <js95112345@gmail.com>
2025-08-16 23:29:16 +0900

    feat: 메인 - product 섹션 초기 값 서버 컴포넌트에서 렌더링

--- apps/web/app/(with-layout)/page.tsx
+++ apps/web/app/(with-layout)/page.tsx
@@ -3,6 +3,8 @@ import { Suspense } from 'react';
 import HomeBanner from './(home)/components/home-banner';
 import HomeSection from './(home)/components/home-section';
 import HomeUpdateDate from './(home)/components/home-update-date';
+import HomeProductFallback from './(home)/components/server-wrapper/home-product-fallback';
+import HomeProductServer from './(home)/components/server-wrapper/home-product-server';
 import HomeReviewFallback from './(home)/components/server-wrapper/home-review-fallback';
 import HomeReviewServer from './(home)/components/server-wrapper/home-review-server';
 import HomeYoutubeFallback from './(home)/components/server-wrapper/home-youtube-fallback';
@@ -16,11 +18,16 @@ export default function Main() {
         <HomeUpdateDate />
         <HomeSection className="mt-[6rem]">
           <HomeSection.Header>レビュー数が多い商品</HomeSection.Header>
-          <HomeSection.Product productSortType="popular" />
+          <Suspense fallback={<HomeProductFallback />}>
+            <HomeProductServer productSortType="popular" />
+          </Suspense>
         </HomeSection>
+
         <HomeSection>
           <HomeSection.Header>新作アイテム</HomeSection.Header>
-          <HomeSection.Product productSortType="new" />
+          <Suspense fallback={<HomeProductFallback />}>
+            <HomeProductServer productSortType="new" />
+          </Suspense>
         </HomeSection>
         <Suspense fallback={<HomeReviewFallback />}>
           <HomeReviewServer />
