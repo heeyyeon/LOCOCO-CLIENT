5cbaf617394d3bc5160ae20686ae83dde987ca14
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-09-01 23:32:29 +0900

    feat: google 로그인 page.tsx 파일 생성

---
+++ apps/web/app/(with-layout)/login-google/page.tsx
@@ -0,0 +1,21 @@
+import LoginButton from './components/loginButton';
+import LoginTitle from './components/loginTitle';
+
+export default function LoginPage({
+  searchParams,
+}: {
+  searchParams: { mode?: string };
+}) {
+  const mode = (searchParams.mode as 'login' | 'signup') || 'login';
+
+  return (
+    <>
+      <main className="mx-auto flex w-screen min-w-[1366px] items-center justify-center bg-gray-100">
+        <section className="mx-auto my-[11.95rem] flex h-[44.2rem] flex-col items-center border border-gray-400 bg-white px-[22.4rem] py-[12rem]">
+          <LoginTitle mode={mode} />
+          <LoginButton />
+        </section>
+      </main>
+    </>
+  );
+}
