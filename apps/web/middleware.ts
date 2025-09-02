91322bdddb386aa068515afe336276b80d6b65fc
ljh0608 <m98m@naver.com>
2025-09-02 11:11:39 +0900

    feat: 특정 지원하지 않는 locale에 대해 en redirect

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -11,7 +11,23 @@ export default function middleware(req: NextRequest) {
   const { origin, pathname } = nextUrl;
 
   // next-intl 미들웨어 실행하여 locale 처리
+
   const response = intlMiddleware(req);
+
+  // 지원하지 않는 locale 접근시 default locale로 리다이렉트
+  const unsupportedLocale = ['fr', 'ru', 'ja', 'zh'];
+  const firstSegment = pathname.split('/')[1];
+  if (firstSegment && unsupportedLocale.includes(firstSegment)) {
+    const pathWithoutUnsupportedLocale = pathname.replace(
+      `/${firstSegment}`,
+      ''
+    );
+    const redirectUrl = new URL(
+      `/${routing.defaultLocale}${pathWithoutUnsupportedLocale}`,
+      process.env.NEXT_PUBLIC_BASE_URL
+    );
+    return NextResponse.redirect(redirectUrl);
+  }
 
   const accessTokenCookie = cookies.get('AccessToken');
   const isLoggedIn = accessTokenCookie !== undefined;
