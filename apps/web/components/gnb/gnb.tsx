312bbac6c96a3cdfd06d40751e55171c59bb16d2
kojesung <js95112345@gmail.com>
2025-09-06 21:19:15 +0900

    fix: Image 태그 CI 경로 문제 해결(import type -> 문자열 경로)

--- apps/web/components/gnb/gnb.tsx
+++ apps/web/components/gnb/gnb.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 
-import logo from '../../public/images/logo.png';
 import GnbAuth from './gnb-auth';
 import GnbLanguage from './gnb-language';
 import GnbMenu from './gnb-menu';
@@ -9,7 +8,7 @@ export default function Gnb() {
   return (
     <header className="inter-body1 flex h-[7.2rem] w-full items-center justify-between px-[11.9rem] font-[700]">
       <Image
-        src={logo}
+        src="/images/logo.png"
         alt="logo"
         width={162}
         height={27}
