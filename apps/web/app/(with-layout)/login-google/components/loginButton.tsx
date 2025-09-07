e66b4b5e0dadae2216ddf36b906ec45cf8d07bcb
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-09-07 19:47:15 +0900

    refactor: div -> button 태그로 수정

--- apps/web/app/(with-layout)/login-google/components/loginButton.tsx
+++ apps/web/app/(with-layout)/login-google/components/loginButton.tsx
@@ -4,13 +4,13 @@ import { SvgGoogle } from '@lococo/icons';
 
 export default function LoginButton() {
   return (
-    <div className="group relative mt-[8rem] flex h-[4.8rem] w-[29.6rem] cursor-pointer items-center rounded-[2.4rem] border border-[#747775] bg-white py-[1.2rem] hover:border-[#747775] hover:bg-[#F7F8F8]">
+    <button className="group relative mt-[8rem] flex h-[4.8rem] w-[29.6rem] cursor-pointer items-center rounded-[2.4rem] border border-[#747775] bg-white py-[1.2rem] hover:border-[#747775] hover:bg-[#F7F8F8]">
       <span className="absolute left-[2rem] flex items-center">
         <SvgGoogle size={20} />
       </span>
       <span className="inter-body1 flex w-full justify-center pl-[2rem] font-bold text-[#1F1F1F]">
         Continue with Google
       </span>
-    </div>
+    </button>
   );
 }
