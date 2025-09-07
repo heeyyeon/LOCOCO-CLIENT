665139695a58a1230ba138698c633d02438fe82a
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-09-07 19:33:46 +0900

    refactor: 불필요한 태그 제거

--- apps/web/app/(with-layout)/login-google/page.tsx
+++ apps/web/app/(with-layout)/login-google/page.tsx
@@ -9,13 +9,11 @@ export default function LoginPage({
   const mode = (searchParams.mode as 'login' | 'signup') || 'login';
 
   return (
-    <>
-      <main className="mx-auto flex w-screen min-w-[1366px] items-center justify-center bg-gray-100">
-        <section className="mx-auto my-[11.95rem] flex h-[44.2rem] w-[74.4rem] flex-col items-center border border-gray-400 bg-white py-[12rem]">
-          <LoginTitle mode={mode} />
-          <LoginButton />
-        </section>
-      </main>
-    </>
+    <main className="mx-auto flex w-screen min-w-[1366px] items-center justify-center bg-gray-100">
+      <section className="mx-auto my-[11.95rem] flex h-[44.2rem] w-[74.4rem] flex-col items-center border border-gray-400 bg-white py-[12rem]">
+        <LoginTitle mode={mode} />
+        <LoginButton />
+      </section>
+    </main>
   );
 }
