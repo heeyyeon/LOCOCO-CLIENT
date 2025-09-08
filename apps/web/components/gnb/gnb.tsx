671af2b36b682f30e7283f2b0c871a2d0d36e41a
kojesung <js95112345@gmail.com>
2025-09-08 10:36:59 +0900

    fix: children 없는 요소 self closing, 불필요한 className 제거

--- apps/web/components/gnb/gnb.tsx
+++ apps/web/components/gnb/gnb.tsx
@@ -7,14 +7,8 @@ import GnbMenu from './gnb-menu';
 export default function Gnb() {
   return (
     <header className="inter-body1 flex h-[7.2rem] w-full items-center justify-between px-[11.9rem] font-[700]">
-      <Image
-        src="/images/logo.png"
-        alt="logo"
-        width={162}
-        height={27}
-        className="h-[27px] w-[162px]"
-      />
-      <GnbMenu></GnbMenu>
+      <Image src="/images/logo.png" alt="logo" width={162} height={27} />
+      <GnbMenu />
       <div className="flex h-full items-center gap-[1.6rem]">
         <GnbAuth />
         <GnbLanguage />
