363706935aba9887d194224c08e8a911f992f4a4
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-09 11:42:19 +0900

    fix: image upload/ preview 분리

--- packages/design-system/src/components/image-upload/ImageUpload.tsx
+++ packages/design-system/src/components/image-upload/ImageUpload.tsx
@@ -1,57 +1,25 @@
 import { ChangeEvent } from 'react';
 
-import { SvgAdd, SvgClose } from '../../icons';
+import { SvgAdd } from '../../icons';
 
 interface ImageUploadProps {
-  src?: string;
-  alt?: string;
   handleFileChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-  handleRemoveFile?: () => void;
-  id?: string;
 }
 
 export default function ImageUpload({
-  src,
-  alt = 'image',
   handleFileChange,
-  handleRemoveFile,
-  id,
+  ...props
 }: ImageUploadProps) {
   return (
-    <>
-      {src ? (
-        <div className="relative h-[7.2rem] w-[7.2rem]">
-          <img
-            src={src}
-            alt={alt}
-            className="h-full w-full rounded-[1.6rem] object-cover"
-          />
-          <input
-            type="file"
-            accept="image/*"
-            className="absolute inset-0 z-10 h-full w-full cursor-pointer opacity-0"
-            onChange={handleFileChange}
-            id={id}
-          />
-          <button
-            onClick={handleRemoveFile}
-            className="absolute bottom-[0.6rem] right-[0.6rem] z-20 flex h-[2rem] w-[2rem] cursor-pointer items-center justify-center rounded-full bg-black/30"
-            type="button"
-          >
-            <SvgClose className="size-[1.6rem] fill-white" />
-          </button>
-        </div>
-      ) : (
-        <div className="flex h-[7.2rem] w-[7.2rem] items-center justify-center rounded-[1.6rem] bg-gray-800">
-          <input
-            type="file"
-            accept="image/*"
-            className="absolute inset-0 z-10 h-full w-full cursor-pointer opacity-0"
-            onChange={handleFileChange}
-          />
-          <SvgAdd size={36} className="cursor-pointer fill-white" />
-        </div>
-      )}
-    </>
+    <div className="flex h-[7.2rem] w-[7.2rem] items-center justify-center rounded-[1.6rem] bg-gray-800">
+      <input
+        type="file"
+        accept="image/*"
+        className="absolute inset-0 z-10 h-full w-full cursor-pointer opacity-0"
+        onChange={handleFileChange}
+        {...props}
+      />
+      <SvgAdd size={36} className="cursor-pointer fill-white" />
+    </div>
   );
 }
