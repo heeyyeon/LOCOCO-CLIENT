1cbc4643826e5e152dc712935df84b617fa88d20
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-10 11:05:41 +0900

    code review: image upload 코드리뷰 반영

--- packages/design-system/src/components/image-upload/ImageUpload.tsx
+++ packages/design-system/src/components/image-upload/ImageUpload.tsx
@@ -1,8 +1,9 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, InputHTMLAttributes } from 'react';
 
 import { SvgAdd } from '../../icons';
 
-interface ImageUploadProps {
+interface ImageUploadProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> {
   handleFileChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
@@ -11,15 +12,16 @@ export default function ImageUpload({
   ...props
 }: ImageUploadProps) {
   return (
-    <div className="flex h-[7.2rem] w-[7.2rem] items-center justify-center rounded-[1.6rem] bg-gray-800">
+    <div className="relative flex h-[7.2rem] w-[7.2rem] items-center justify-center rounded-[1.6rem] bg-gray-800 p-[1.8rem]">
       <input
         type="file"
         accept="image/*"
         className="absolute inset-0 z-10 h-full w-full cursor-pointer opacity-0"
         onChange={handleFileChange}
+        aria-label="image upload"
         {...props}
       />
-      <SvgAdd size={36} className="cursor-pointer fill-white" />
+      <SvgAdd size={36} fill="white" />
     </div>
   );
 }
