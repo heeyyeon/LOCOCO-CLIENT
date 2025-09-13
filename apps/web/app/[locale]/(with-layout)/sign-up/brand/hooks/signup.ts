9c220dd1905a68597e0135f4c58a62b94b100ef7
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-09-13 18:44:05 +0900

    refactor: 에러 메시지 문장 수정

--- apps/web/app/[locale]/(with-layout)/sign-up/brand/hooks/signup.ts
+++ apps/web/app/[locale]/(with-layout)/sign-up/brand/hooks/signup.ts
@@ -7,32 +7,32 @@ export const brandSignupSchema = z
     brandName: z
       .string()
       .trim()
-      .min(1, '브랜드 이름을 입력해주세요')
-      .max(15, '15자 이하로 입력해주세요')
-      .regex(/^[a-zA-Z가-힣\s]+$/, '한글 또는 영어만 입력해주세요'),
+      .min(1, '브랜드 이름을 입력해주세요.')
+      .max(30, '브랜드 이름은 최대 30글자입니다.')
+      .regex(/^[a-zA-Z가-힣\s]+$/, '한글 또는 영어만 입력해주세요.'),
     contactName: z
       .string()
       .trim()
-      .min(1, '담당자 이름을 입력해주세요')
-      .max(10, '10자 이하로 입력해주세요')
-      .regex(/^[가-힣]+$/, '한글만 입력해주세요'),
+      .min(1, '담당자 이름을 입력해주세요.')
+      .max(10, '담당자 이름은 최대 10글자입니다.')
+      .regex(/^[가-힣]+$/, '한글만 입력해주세요.'),
     contactPosition: z
       .string()
       .trim()
       .min(1, '담당자 직책을 입력해주세요')
-      .max(10, '10자 이하로 입력해주세요')
+      .max(10, '담당자 직책은 최대 10글자입니다.')
       .regex(/^[가-힣]+$/, '한글만 입력해주세요'),
     contactPhone: z
       .string()
       .trim()
-      .min(1, '담당자 연락처를 입력해주세요')
-      .refine(isValidPhoneNumber, '올바른 휴대폰 번호를 입력해주세요'),
-    street: z.string().trim().min(1, '도로명주소를 입력해주세요'),
+      .min(1, '담당자 연락처를 입력해주세요.')
+      .refine(isValidPhoneNumber, '올바른 휴대폰 번호를 입력해주세요.'),
+    street: z.string().trim().min(1, '도로명주소를 입력해주세요.'),
     detail: z
       .string()
       .trim()
-      .min(1, '상세주소를 입력해주세요')
-      .max(30, '30자 이하로 입력해주세요'),
+      .min(1, '상세주소를 입력해주세요.')
+      .max(30, '상세주소는 최대 30글자입니다.'),
   })
   .strict();
 
