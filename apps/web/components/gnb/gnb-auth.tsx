2156e026b2775423f7aeafc377463fc038e57dbe
kojesung <js95112345@gmail.com>
2025-09-17 12:48:27 +0900

    feat: translate 파일 다운로드, gnb-auth 파일 button -> Link 변경

--- apps/web/components/gnb/gnb-auth.tsx
+++ apps/web/components/gnb/gnb-auth.tsx
@@ -3,6 +3,7 @@
 import { useTranslations } from 'next-intl';
 
 import { useAuth } from 'hooks/use-auth';
+import { Link } from 'i18n/navigation';
 
 import { SvgProfileIcon } from '@lococo/icons';
 
@@ -16,15 +17,19 @@ import {
 export default function GnbAuth() {
   const { isLoggedIn } = useAuth();
   const t = useTranslations('gnb');
+  // TODO Link태그 href 경로 논의 필요
   return (
     <div className="flex h-[5.6rem] items-center gap-4">
       {isLoggedIn ? (
         <DropdownMenu>
           <DropdownMenuTrigger asChild className="relative">
-            <button className="flex h-[5.6rem] items-center gap-[1rem] px-[1rem] py-[1.6rem] text-black">
+            <Link
+              href={'/myPage'}
+              className="flex h-[5.6rem] items-center gap-[1rem] px-[1rem] py-[1.6rem] text-black"
+            >
               <SvgProfileIcon size={20} />
               <span>유저이름</span>
-            </button>
+            </Link>
           </DropdownMenuTrigger>
 
           <DropdownMenuContent
@@ -37,10 +42,12 @@ export default function GnbAuth() {
         </DropdownMenu>
       ) : (
         <>
-          <button className="px-[1.6rem] py-[1rem]">{t('login')}</button>
-          <button className="px-[1.6rem] py-[1rem] text-pink-500">
+          <Link href={'/login'} className="px-[1.6rem] py-[1rem]">
+            {t('logIn')}
+          </Link>
+          <Link href={'signup'} className="px-[1.6rem] py-[1rem] text-pink-500">
             {t('signUp')}
-          </button>
+          </Link>
         </>
       )}
     </div>
