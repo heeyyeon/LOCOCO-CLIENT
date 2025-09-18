6b30c56a54f57c91d5cf8c1c64b380fe4817e456
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-09-18 21:51:36 +0900

    feat: SelectFormField 컴포넌트 index.ts 파일로 export

--- apps/web/components/forms/index.ts
+++ apps/web/components/forms/index.ts
@@ -2,3 +2,4 @@ export { SignupFormLayout } from './SignupFormLayout';
 export { FormSection } from './FormSection';
 export { TextFormField } from './TextFormField';
 export { PhoneFormField } from './PhoneFormField';
+export { SelectFormField } from './SelectFormField';
