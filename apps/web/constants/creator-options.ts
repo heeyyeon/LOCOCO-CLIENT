ea20cbceabda3cb7999d8eec49b56bcc81d5b334
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-09-18 21:49:31 +0900

    feat: 크리에이터 폼 페이지 select 옵션 상수화

---
+++ apps/web/constants/creator-options.ts
@@ -0,0 +1,28 @@
+export const GENDERS = [
+  { label: 'Male', value: 'male' },
+  { label: 'Female', value: 'female' },
+  { label: 'Non-binary', value: 'non-binary' },
+  { label: 'Prefer not to say', value: 'prefer not to say' },
+];
+
+export const CONTENT_LANGUAGES = [
+  { label: 'English', value: 'en' },
+  { label: 'Spanish', value: 'es' },
+  { label: 'English & Spanish', value: 'en_es' },
+];
+
+export const SKIN_TYPES = [
+  { label: 'Normal', value: 'normal' },
+  { label: 'Dry', value: 'dry' },
+  { label: 'Oily', value: 'oily' },
+  { label: 'Combination', value: 'combination' },
+  { label: 'Sensitive', value: 'sensitive' },
+  { label: 'Others', value: 'others' },
+];
+
+export const SKIN_TONE_DATA = [
+  { label: 'Shade 1', value: 'shade 1', color: '#F1DDD2' },
+  { label: 'Shade 2', value: 'shade 2', color: '#F4D6CB' },
+  { label: 'Shade 3', value: 'shade 3', color: '#EBC9BA' },
+  { label: 'Shade 4', value: 'shade 4', color: '#EDCBBC' },
+];
