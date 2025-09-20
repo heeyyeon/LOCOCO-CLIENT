7a6f7da1c7a41355491387744d128cb768e3cc82
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-20 20:05:57 +0900

    fixL addressmodal 삭제

--- apps/web/app/[locale]/(with-layout)/my-page/@modal/(.)address-modal/AddressModal.tsx
+++ apps/web/app/[locale]/(with-layout)/my-page/@modal/(.)address-modal/AddressModal.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 
 import { useTranslations } from 'next-intl';
 import { useRouter } from 'next/navigation';
@@ -8,15 +8,8 @@ import {
   DialogContent,
   DialogTitle,
 } from '@lococo/design-system/dialog';
-import { ErrorNotice } from '@lococo/design-system/error-notice';
-import { Input } from '@lococo/design-system/input';
 import { ModalButton } from '@lococo/design-system/modal-button';
 import { ModalHeader } from '@lococo/design-system/modal-header';
-import { Select } from '@lococo/design-system/select';
-
-import { useProfile } from '../../hooks/use-profile';
-import InputWrapper from './components/InputWrapper';
-import { useAddress } from './hooks/useAddress';
 
 interface AddressModalProps {
   open: boolean;
@@ -27,53 +20,14 @@ export function AddressModal({ open, onOpenChange }: AddressModalProps) {
   const router = useRouter();
   const t = useTranslations('myPage.addressModal');
 
-  const { formData: profileData } = useProfile();
-
-  const addressData = useMemo(
-    () => ({
-      country: profileData.country || '',
-      state: profileData.state || '',
-      city: profileData.city || '',
-      addressLine1: profileData.addressLine1 || '',
-      addressLine2: profileData.addressLine2 || '',
-      zip: profileData.zip || '',
-    }),
-    [
-      profileData.country,
-      profileData.state,
-      profileData.city,
-      profileData.addressLine1,
-      profileData.addressLine2,
-      profileData.zip,
-    ]
-  );
-
-  const {
-    formData,
-    errors,
-    updateCountry,
-    updateState,
-    updateCity,
-    updateAddressLine1,
-    updateAddressLine2,
-    updateZip,
-    handleSubmit,
-    isFormValid,
-    trigger,
-  } = useAddress(undefined, undefined, addressData);
-
   const handleEditModal = () => {
     onOpenChange(false);
     router.push('/my-page?tab=edit-profile&returnTo=address-modal');
-    handleSubmit();
   };
 
   const handleGetDeliveryModal = () => {
-    trigger();
+    onOpenChange(false);
     // TODO: 모달 닫히고 마이페이지의 캠페인 카드 버튼이 confirm address에서 upload 1st review로 바뀜
-    if (isFormValid) {
-      onOpenChange(false);
-    }
   };
 
   return (
@@ -91,85 +45,7 @@ export function AddressModal({ open, onOpenChange }: AddressModalProps) {
             <p className="title2 text-gray-800">{t('title')}</p>
             <p className="caption3 text-gray-500">{t('description')}</p>
           </div>
-
-          <InputWrapper label={t('country')} required>
-            {
-              //TODO: 국가 목록 추가
-            }
-            <Select
-              options={[{ label: 'United States' }, { label: 'Canada' }]}
-              onValueChange={(value) => updateCountry(value)}
-              placeholder="Country"
-              value={formData.country}
-              isError={!!errors.country}
-              errorText={errors.country}
-              className="w-full"
-            />
-          </InputWrapper>
-          <InputWrapper
-            label={t('state')}
-            required
-            notice={errors.state && <ErrorNotice message={errors.state} />}
-          >
-            <Input
-              value={formData.state}
-              onChange={(e) => updateState(e.target.value)}
-              className="w-full"
-            />
-          </InputWrapper>
-          <InputWrapper
-            label={t('city')}
-            required
-            notice={errors.city && <ErrorNotice message={errors.city} />}
-          >
-            <Input
-              value={formData.city}
-              onChange={(e) => updateCity(e.target.value)}
-              className="w-full"
-            />
-          </InputWrapper>
-          <InputWrapper
-            label={t('addressLine1')}
-            required
-            notice={
-              errors.addressLine1 && (
-                <ErrorNotice message={errors.addressLine1} />
-              )
-            }
-          >
-            <Input
-              value={formData.addressLine1}
-              onChange={(e) => updateAddressLine1(e.target.value)}
-              className="w-full"
-            />
-          </InputWrapper>
-          <InputWrapper
-            label={t('addressLine2')}
-            notice={
-              errors.addressLine2 && (
-                <ErrorNotice message={errors.addressLine2} />
-              )
-            }
-          >
-            <Input
-              value={formData.addressLine2}
-              onChange={(e) => updateAddressLine2(e.target.value)}
-              className="w-full"
-            />
-          </InputWrapper>
-
-          <InputWrapper
-            label={t('zip')}
-            notice={errors.zip && <ErrorNotice message={errors.zip} />}
-          >
-            <Input
-              value={formData.zip}
-              onChange={(e) => updateZip(e.target.value)}
-              className="w-full"
-            />
-          </InputWrapper>
         </section>
-
         <div className="flex items-center border-t border-pink-500">
           <ModalButton
             text={t('edit')}
