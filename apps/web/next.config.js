c543e45ddbe73052acbd1d49ecbfae853af18081
kojesung <js95112345@gmail.com>
2025-09-20 18:54:05 +0900

    feat: auth 관련 기능 개발 전 테스트를 위한 프록시(요청 도메인 변경) 사용

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -16,6 +16,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
