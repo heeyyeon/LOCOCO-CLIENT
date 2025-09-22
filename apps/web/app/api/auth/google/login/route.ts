2dcd613cc4ad048841e21a87f8e38c298fabee62
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-09-22 21:19:25 +0900

    feat: 토큰 획득 후 리다이렉트 url 로딩 페이지로 변경

--- apps/web/app/api/auth/google/login/route.ts
+++ apps/web/app/api/auth/google/login/route.ts
@@ -25,7 +25,7 @@ export async function GET(request: NextRequest) {
 
       if (accessToken) {
         const redirectResponse = NextResponse.redirect(
-          new URL('/', request.url)
+          new URL('/login-google/loading', request.url)
         );
 
         redirectResponse.cookies.set('AccessToken', accessToken, {
