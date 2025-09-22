58b7fc1a0202ad3788846bcd08e62c055b178502
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-09-23 00:23:52 +0900

    refactor: 로그인 버튼 태그 Link 태그로 수정

--- apps/web/app/[locale]/(with-layout)/login-google/components/loginButton.tsx
+++ apps/web/app/[locale]/(with-layout)/login-google/components/loginButton.tsx
@@ -1,19 +1,18 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 
 import { SvgGoogle } from '@lococo/icons';
 
 export default function LoginButton() {
   const t = useTranslations('loginGoogle');
 
-  const handleGoogleLogin = () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/auth/google/redirect`;
-  };
+  const googleLoginUrl = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/auth/google/redirect`;
 
   return (
-    <button
-      onClick={handleGoogleLogin}
+    <Link
+      href={googleLoginUrl}
       className="group relative mt-[8rem] flex h-[4.8rem] w-[29.6rem] cursor-pointer items-center rounded-[2.4rem] border border-[#747775] bg-white py-[1.2rem] hover:border-[#747775] hover:bg-[#F7F8F8]"
     >
       <span className="absolute left-[2rem] flex items-center">
@@ -22,6 +21,6 @@ export default function LoginButton() {
       <span className="body1 flex w-full justify-center pl-[2rem] font-bold text-[#1F1F1F]">
         {t('continueWithGoogle')}
       </span>
-    </button>
+    </Link>
   );
 }
