2ef64f77b6ebf53fa4c4e998c1817f8071f1f92b
kojesung <js95112345@gmail.com>
2025-09-24 02:34:46 +0900

    feat: 영상 배너 추가

--- apps/web/app/[locale]/(with-layout)/(home)/components/home-banner.tsx
+++ apps/web/app/[locale]/(with-layout)/(home)/components/home-banner.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-import Image from 'next/image';
-
 export default function HomeBanner() {
-  //TODO 배너 나오면 수정, alt에 번역키 적용
   return (
-    <div className="w-full bg-pink-200">
+    <div className="w-full bg-white">
       <div className="flex min-w-[1366px] justify-center">
-        <Image
-          src="/images/home-banner.png"
-          alt="배너"
+        <video
+          src="/video/home-banner-video.mp4"
           width={1366}
           height={560}
+          autoPlay
+          muted
+          loop
+          playsInline
           className="block"
-          priority
+          preload="metadata"
         />
       </div>
     </div>
