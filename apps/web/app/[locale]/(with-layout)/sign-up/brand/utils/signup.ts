754ad7e1009185a41bd78eb633ed37048c7e6952
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-09-27 04:02:45 +0900

    feat: 다국어 키 적용

--- apps/web/app/[locale]/(with-layout)/sign-up/brand/utils/signup.ts
+++ apps/web/app/[locale]/(with-layout)/sign-up/brand/utils/signup.ts
@@ -2,41 +2,41 @@ import { z } from 'zod';
 
 import { isValidPhoneNumber } from '../../../../../../utils/format-phone-number';
 
-export const brandSignupSchema = z
-  .object({
-    brandName: z
-      .string()
-      .trim()
-      .min(1, '브랜드 이름을 입력해주세요.')
-      .max(30, '브랜드 이름은 최대 30글자입니다.')
-      .regex(
-        /^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9\s]+$/,
-        '한글, 영어, 숫자만 입력해주세요.'
-      ),
-    contactName: z
-      .string()
-      .trim()
-      .min(1, '담당자 이름을 입력해주세요.')
-      .max(10, '담당자 이름은 최대 10글자입니다.')
-      .regex(/^[가-힣ㄱ-ㅎㅏ-ㅣ]+$/, '한글만 입력해주세요.'),
-    contactPosition: z
-      .string()
-      .trim()
-      .min(1, '담당자 직책을 입력해주세요')
-      .max(10, '담당자 직책은 최대 10글자입니다.')
-      .regex(/^[가-힣ㄱ-ㅎㅏ-ㅣ]+$/, '한글만 입력해주세요'),
-    contactPhone: z
-      .string()
-      .trim()
-      .min(1, '담당자 연락처를 입력해주세요.')
-      .refine(isValidPhoneNumber, '올바른 휴대폰 번호를 입력해주세요.'),
-    street: z.string().trim().min(1, '도로명주소를 입력해주세요.'),
-    detail: z
-      .string()
-      .trim()
-      .min(1, '상세주소를 입력해주세요.')
-      .max(30, '상세주소는 최대 30글자입니다.'),
-  })
-  .strict();
+type TranslationFunction = (key: string) => string;
 
-export type BrandSignupForm = z.infer<typeof brandSignupSchema>;
+export const brandSignupSchema = (t: TranslationFunction) =>
+  z
+    .object({
+      brandName: z
+        .string()
+        .trim()
+        .min(1, t('brandNameRequired'))
+        .max(30, t('brandNameMaxLength'))
+        .regex(/^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9\s]+$/, t('brandNameInvalidFormat')),
+      contactName: z
+        .string()
+        .trim()
+        .min(1, t('contactNameRequired'))
+        .max(10, t('contactNameMaxLength'))
+        .regex(/^[가-힣ㄱ-ㅎㅏ-ㅣ]+$/, t('contactNameInvalidFormat')),
+      contactPosition: z
+        .string()
+        .trim()
+        .min(1, t('contactPositionRequired'))
+        .max(10, t('contactPositionMaxLength'))
+        .regex(/^[가-힣ㄱ-ㅎㅏ-ㅣ]+$/, t('contactPositionInvalidFormat')),
+      contactPhone: z
+        .string()
+        .trim()
+        .min(1, t('contactPhoneRequired'))
+        .refine(isValidPhoneNumber, t('contactPhoneInvalid')),
+      street: z.string().trim().min(1, t('streetAddressRequired')),
+      detail: z
+        .string()
+        .trim()
+        .min(1, t('detailAddressRequired'))
+        .max(30, t('detailAddressMaxLength')),
+    })
+    .strict();
+
+export type BrandSignupForm = z.infer<ReturnType<typeof brandSignupSchema>>;
