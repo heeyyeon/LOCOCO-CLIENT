90e67cb270f252ed0002662b81860e017fb600d3
kojesung <js95112345@gmail.com>
2025-09-28 19:31:06 +0900

    feat: empty 컴포넌트

---
+++ apps/web/components/empty/campgin-list-empty.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { SvgEmptyImage } from '@lococo/icons';
+import { cn } from '@lococo/utils';
+
+interface CampaignListEmptyProps {
+  emptyMessage: string;
+  className?: string;
+}
+
+export default function CampaignListEmpty({
+  emptyMessage,
+  className,
+}: CampaignListEmptyProps) {
+  return (
+    <section
+      className={cn(
+        'flex h-[69.4rem] w-[112.8rem] items-center justify-center gap-[3.2rem]',
+        className
+      )}
+    >
+      <SvgEmptyImage size={120} />
+      <p className="title2 font-[700] text-gray-700">{emptyMessage}</p>
+    </section>
+  );
+}
