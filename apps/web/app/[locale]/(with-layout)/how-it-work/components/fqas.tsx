a79bafe02aa852520dc396c2406c263e449e1b39
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-29 15:31:25 +0900

    fix: 번역 키 수정

--- apps/web/app/[locale]/(with-layout)/how-it-work/components/fqas.tsx
+++ apps/web/app/[locale]/(with-layout)/how-it-work/components/fqas.tsx
@@ -8,71 +8,65 @@ import QAndA from './q-and-a';
 
 export default function FQAs() {
   const t = useTranslations('howItWork.FAQs');
+
   return (
     <div className="flex flex-col gap-[4.8rem]">
       <p className="head1 font-[700] text-pink-500">{t('title')}</p>
-      <div className="flex flex-col gap-[4rem]">
-        <InfoChip
-          text={t('creatorLevels.title')}
-          size="lg"
-          color="default"
-          className="w-fit"
+      <FQAsItem infoChipText={t('creatorLevels.title')}>
+        <QAndA
+          question={t('creatorLevels.firstQ')}
+          answer={t('creatorLevels.firstA')}
         />
-        <div className="flex flex-col gap-[3.2rem]">
-          <QAndA
-            question={t('creatorLevels.fistQ')}
-            answer={t('creatorLevels.fistA')}
-          />
-          <QAndA
-            question={t('creatorLevels.secondQ')}
-            answer={t('creatorLevels.secondA')}
-          />
-          <QAndA
-            question={t('creatorLevels.thirdQ')}
-            answer={t('creatorLevels.thirdA')}
-          />
-        </div>
-      </div>
-      <div className="flex flex-col gap-[4rem]">
-        <InfoChip
-          text={t('shipping.title')}
-          size="lg"
-          color="default"
-          className="w-fit"
+        <QAndA
+          question={t('creatorLevels.secondQ')}
+          answer={t('creatorLevels.secondA')}
         />
-        <div className="flex flex-col gap-[3.2rem]">
-          <QAndA
-            question={t('shipping.firstQ')}
-            answer={t('shipping.firstA')}
-          />
-          <QAndA
-            question={t('shipping.secondQ')}
-            answer={t('shipping.secondA')}
-          />
-        </div>
-      </div>
-      <div className="flex flex-col gap-[4rem]">
-        <InfoChip
-          text={t('penaltyPolicy.title')}
-          size="lg"
-          color="default"
-          className="w-fit"
+        <QAndA
+          question={t('creatorLevels.thirdQ')}
+          answer={t('creatorLevels.thirdA')}
         />
-        <div className="flex flex-col gap-[3.2rem]">
-          <QAndA
-            question={t('penaltyPolicy.firstQ')}
-            answer={t('penaltyPolicy.firstA')}
-          />
-          <QAndA
-            question={t('penaltyPolicy.secondQ')}
-            answer={t('penaltyPolicy.secondA')}
-          />
-          <QAndA
-            question={t('penaltyPolicy.thirdQ')}
-            answer={t('penaltyPolicy.thirdA')}
-          />
-        </div>
-      </div>
+      </FQAsItem>
+      <FQAsItem infoChipText={t('shipping.title')}>
+        <QAndA question={t('shipping.firstQ')} answer={t('shipping.firstA')} />
+        <QAndA
+          question={t('shipping.secondQ')}
+          answer={t('shipping.secondA')}
+        />
+      </FQAsItem>
+      <FQAsItem infoChipText={t('penaltyPolicy.title')}>
+        <QAndA
+          question={t('penaltyPolicy.firstQ')}
+          answer={t('penaltyPolicy.firstA')}
+        />
+        <QAndA
+          question={t('penaltyPolicy.secondQ')}
+          answer={t('penaltyPolicy.secondA')}
+        />
+        <QAndA
+          question={t('penaltyPolicy.thirdQ')}
+          answer={t('penaltyPolicy.thirdA')}
+        />
+      </FQAsItem>
+    </div>
+  );
+}
+
+function FQAsItem({
+  infoChipText,
+  children,
+}: {
+  infoChipText: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col gap-[4rem]">
+      <InfoChip
+        text={infoChipText}
+        size="lg"
+        color="default"
+        className="w-fit"
+      />
+      <div className="flex flex-col gap-[3.2rem]">{children}</div>
     </div>
   );
 }
