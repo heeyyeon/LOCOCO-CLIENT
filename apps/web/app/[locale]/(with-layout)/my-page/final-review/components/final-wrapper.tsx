040c21ae9437f6a2c157c7230d3239002f3b3803
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-30 15:35:00 +0900

    fix: lint error

--- apps/web/app/[locale]/(with-layout)/my-page/final-review/components/final-wrapper.tsx
+++ apps/web/app/[locale]/(with-layout)/my-page/final-review/components/final-wrapper.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 interface FinalWrapperProps {
   title: string;
   children: React.ReactNode;
--- apps/web/app/[locale]/(with-layout)/my-page/final-review/components/image-box.tsx
+++ apps/web/app/[locale]/(with-layout)/my-page/final-review/components/image-box.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import Image from 'next/image';
 
