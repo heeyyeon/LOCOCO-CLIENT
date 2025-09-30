cc874ff124128f011801d5a94d6dbbba604fbfc7
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-30 15:32:59 +0900

    feat: 최종 리뷰 확인 컴포넌트 생성

---
+++ apps/web/app/[locale]/(with-layout)/my-page/final-review/components/final-wrapper.tsx
@@ -0,0 +1,13 @@
+interface FinalWrapperProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export default function FinalWrapper({ title, children }: FinalWrapperProps) {
+  return (
+    <div className="flex w-full flex-col items-start gap-[1.6rem]">
+      <p className="title2 font-bold text-gray-800">{title}</p>
+      {children}
+    </div>
+  );
+}
