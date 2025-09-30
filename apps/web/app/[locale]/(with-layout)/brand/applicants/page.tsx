5347e2d98d608abe66f938f81da68293715d1aca
ljh0608 <m98m@naver.com>
2025-09-30 21:50:49 +0900

    feat: 브랜드 리스트 조회 api 추가

--- apps/web/app/[locale]/(with-layout)/brand/applicants/page.tsx
+++ apps/web/app/[locale]/(with-layout)/brand/applicants/page.tsx
@@ -1,5 +1,19 @@
-import ClientPage from './page.client';
+import { notFound } from 'next/navigation';
 
-export default function page() {
-  return <ClientPage />;
+import { getBrandCampaignInfos } from './apis';
+import ClientPage from './page.client';
+import { CampaignInfo } from './types';
+
+export default async function page() {
+  let campaignInfos: CampaignInfo[] = [];
+
+  try {
+    const response = await getBrandCampaignInfos();
+    campaignInfos = response.data.campaignInfos;
+  } catch {
+    // TODO: 에러 핸들링 로직 추가
+    notFound();
+  }
+
+  return <ClientPage campaignInfos={campaignInfos} />;
 }
