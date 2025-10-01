7fb0db6f6305f8e493b0668d45e79bd29e050726
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-01 21:11:45 +0900

    fix: brand 문제 해결

--- apps/web/app/[locale]/(with-layout)/brand/feedback/constant/queryKey.ts
+++
@@ -1,5 +0,0 @@
-export const BRAND_FEEDBACK_KEYS = {
-  all: ['brandFeedback'] as const,
-  review: (campaignReviewId: number) =>
-    [...BRAND_FEEDBACK_KEYS.all, 'review', campaignReviewId] as const,
-};
