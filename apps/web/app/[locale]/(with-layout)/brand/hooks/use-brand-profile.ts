1f11646b2b4d43fc7a918ead708c726c85080ecb
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-01 20:59:52 +0900

    fix: header token 제거

--- apps/web/app/[locale]/(with-layout)/brand/hooks/use-brand-profile.ts
+++ apps/web/app/[locale]/(with-layout)/brand/hooks/use-brand-profile.ts
@@ -4,15 +4,10 @@ import { ApiResponseBrandMyPageResponse } from 'swagger-codegen/data-contracts';
 
 import { BRAND_PROFILE_KEYS } from '../constant/querykey';
 
-const headers = {
-  'Content-Type': 'application/json',
-  Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}`,
-};
 export const useBrandProfile = () => {
   const getBrandProfile = async () => {
     const response = await apiRequest<ApiResponseBrandMyPageResponse>({
       endPoint: '/api/brands/profile',
-      headers,
       method: 'GET',
     });
     return response;
