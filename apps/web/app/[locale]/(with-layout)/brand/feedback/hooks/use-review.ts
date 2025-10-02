f7fe46377720eb6cda473bef686428e50fcb963a
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-02 13:53:05 +0900

    feat: feedback 저장, 전달 api

--- apps/web/app/[locale]/(with-layout)/brand/feedback/hooks/use-review.ts
+++ apps/web/app/[locale]/(with-layout)/brand/feedback/hooks/use-review.ts
@@ -7,18 +7,12 @@ import {
 } from '../../../../../../swagger-codegen/data-contracts';
 import { BRAND_FEEDBACK_KEYS } from '../../constant/querykey';
 
-const headers = {
-  'Content-Type': 'application/json',
-  Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}`,
-};
-
 export const useGetReview = (campaignReviewId: number) => {
   const fetchReview = async (campaignReviewId: number) => {
     const response =
       await apiRequest<ApiResponseCampaignReviewDetailListResponse>({
         endPoint: `/api/brands/my/campaigns/creators/${campaignReviewId}/review`,
         method: 'GET',
-        headers,
       });
     return response;
   };
@@ -28,17 +22,17 @@ export const useGetReview = (campaignReviewId: number) => {
   });
 };
 
-export const useSBrandNote = (campaignReviewId: number, brandNote: string) => {
-  const saveBrandNote = async (campaignReviewId: number, brandNote: string) => {
+export const useBrandNote = (campaignReviewId: number, brandNote: string) => {
+  const saveBrandNote = async (action: 'SAVE_DRAFT' | 'SUBMIT') => {
     const response = await apiRequest<ApiResponseBrandNoteRevisionResponse>({
       endPoint: `/api/brands/my/reviews/${campaignReviewId}/revision-request`,
       method: 'POST',
-      headers,
+      params: { action: action, campaignReviewId: campaignReviewId.toString() },
       data: { brandNote },
     });
     return response;
   };
   return useMutation({
-    mutationFn: () => saveBrandNote(campaignReviewId, brandNote),
+    mutationFn: (action: 'SAVE_DRAFT' | 'SUBMIT') => saveBrandNote(action),
   });
 };
