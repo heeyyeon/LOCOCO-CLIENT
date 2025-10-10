81e3dc8e1cf0aef324aafae60e5e2b6991b8b3ac
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-10 10:22:47 +0900

    feat: 쿼리키 무효화 추가

--- apps/web/app/[locale]/(with-layout)/campaign-detail/[campaignId]/apis/index.ts
+++ apps/web/app/[locale]/(with-layout)/campaign-detail/[campaignId]/apis/index.ts
@@ -1,5 +1,7 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { apiRequest } from 'app/api/apiRequest';
 
+import { CAMPAIGN_REVIEW_KEYS } from '../../../my-page/constant/queryKey';
 import { CampaignDetailResponse, CreatorCampaignApplyResponse } from '../types';
 
 export const getCampaignDetail = async (campaignId: string) => {
@@ -17,6 +19,7 @@ export const getCampaignDetail = async (campaignId: string) => {
 
 // 지원자 수용인원 full / 이미 지원한 캠페인 - 409 대응 필요
 export const applyCampaign = async (campaignId: string) => {
+  const queryClient = useQueryClient();
   const response = await apiRequest<CreatorCampaignApplyResponse>({
     endPoint: `/api/creator-campaign/${campaignId}/participate`,
     method: 'POST',
@@ -25,4 +28,12 @@ export const applyCampaign = async (campaignId: string) => {
     alert('Campaign already applied');
     throw new Error('Campaign already applied');
   }
+
+  if (queryClient) {
+    queryClient.invalidateQueries({
+      queryKey: CAMPAIGN_REVIEW_KEYS.all,
+    });
+  }
+
+  return response;
 };
